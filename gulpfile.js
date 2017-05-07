6231e19dc3c6b2037b873cdf8fb1b66684bc826b
timolawl <timolawl@gmail.com>
2017-05-07 01:39:49 -0700

    add gulp build-css task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@
 // General
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const plumber = require('gulp-plumber');
 
 // JSX/ES6 -> ES5
 const browserify = require('browserify');
@@ -17,14 +18,28 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const es = require('event-stream');
 
+// SCSS -> CSS
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+
+
+// Configuration Objects
+// ---------------------
+
 const paths = {
   // scripts: ['./client/scripts/**/*.jsx', './client/scripts/**/*.js'],
   entries: ['./App.jsx']
 };
 
+
+// Gulp Tasks
+// ----------
+
 gulp.task('build-js', () => {
   const streams = paths.entries.map(entry =>
     browserify({ entries: [entry], basedir: './client/scripts', extensions: ['.jsx'], debug: true })
+      .pipe(plumber())
       .transform(babelify)
       .bundle()
       .on('error', gutil.log.bind(gutil, 'Browserify error.'))
@@ -42,3 +57,12 @@ gulp.task('build-js', () => {
 
   return es.merge.apply(null, streams);
 });
+
+gulp.task('build-css', () =>
+  gulp.src('./client/stylesheets/*.scss') // only the entry/index sheet, style.scss
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssmin())
+    .pipe(gulp.dest('./public/css'))
+);
