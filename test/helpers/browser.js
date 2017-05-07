9c02c60d765d0b2beffe1317c4102bb674004d24
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-07 15:17:59 +0300

    Removed babel-register (included in mocha --compilers. Added jsdom + global initialisation.

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -1,3 +1,15 @@
+/* browser.js
+ *
+ * Sets up 'browser-like' environment for running unit tests for React
+ * components.
+ * */
 
-require('babel-register')();
+import { JSDOM } from 'jsdom';
+
+const dom = new JSDOM('<html><head></head><body></body></html>');
+global.document = dom;
+
+global.navigator = {
+  userAgent: 'node.js'
+};
 
