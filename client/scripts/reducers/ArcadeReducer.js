5cdbb770c5d292803aa347fe6ce7b11b4a22b845
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-08 15:06:15 +0300

    Added import for UserData and handling of RUN_TEST action.

--- client/scripts/reducers/ArcadeReducer.js
+++ client/scripts/reducers/ArcadeReducer.js
@@ -1,8 +1,27 @@
 
 'use strict';
 
+import { RUN_TEST } from '../actions/ArcadeAction';
+import UserData from '../model/UserData';
+
 export default function arcadeReducer(state, action) {
   if (typeof state === 'undefined') {
-    return {};
+    return {
+      isRunningTests: false,
+      userData: new UserData({ username: '' })
+    };
   }
+
+  const nextState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RUN_TEST: {
+      nextState.isRunningTests = true;
+      break;
+    }
+
+    default: console.log('Default reached.');
+  }
+
+  return nextState;
 }
