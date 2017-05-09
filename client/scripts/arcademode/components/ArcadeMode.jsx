c44f13513c979cc29778936b0bb4692f4a4c5bd5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-09 15:09:50 +0300

    Added <CodeMirror>, editorOptions and onCodeChange()

--- client/scripts/arcademode/components/ArcadeMode.jsx
+++ client/scripts/arcademode/components/ArcadeMode.jsx
@@ -3,8 +3,27 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import CodeMirror from 'react-codemirror';
+
 import Hello from './Hello';
 import UserData from '../model/UserData';
+import CodeRetVal from '../model/CodeRetVal';
+
+const editorOptions = {
+  theme: 'monokai',
+  scrollbarStyle: 'null',
+  lineWrapping: true,
+  mode: 'javascript',
+  matchBrackets: true,
+  autoCloseBrackets: true,
+  /*
+  lineNumbers: true, // seems to break the css/dimensions
+  lint: { esversion: 6 },
+  runnable: true,
+  gutters: ['CodeMirror-lint-markers']
+  inputStyle: 'contenteditable'
+  */
+};
 
 /**
  * Top-level component for the app. This is rendered in App.jsx.
@@ -14,6 +33,13 @@ export default class ArcadeMode extends Component {
   constructor(props) {
     super(props);
     this.onClickCallback = this.onClickCallback.bind(this);
+
+    this.onCodeChange = this.onCodeChange.bind(this);
+  }
+
+  onCodeChange(newCode) {
+    console.log('Emitting new code from <ArcadeMode>');
+    this.props.onCodeChange(newCode);
   }
 
   onClickCallback(e) {
@@ -28,6 +54,16 @@ export default class ArcadeMode extends Component {
         <Hello />
         <button onClick={this.onClickCallback}>Run</button>
         <p>Userdata given: {this.props.userData.username} </p>
+        <p>Your code returned: {this.props.codeRetVal.toString()}</p>
+
+        <div className={'editor'}>
+          <CodeMirror
+            onChange={this.onCodeChange}
+            options={editorOptions}
+            value={this.props.code}
+          />
+        </div>
+
       </div>
     );
   }
@@ -35,6 +71,9 @@ export default class ArcadeMode extends Component {
 }
 
 ArcadeMode.propTypes = {
+  code: PropTypes.string.isRequired,
+  onCodeChange: PropTypes.func.isRequired,
+  codeRetVal: PropTypes.instanceOf(CodeRetVal).isRequired,
   runTest: PropTypes.func.isRequired,
   userData: PropTypes.instanceOf(UserData).isRequired
 };
