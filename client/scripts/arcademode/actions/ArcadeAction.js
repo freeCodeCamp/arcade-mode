5d332b14bc3f8d924999d8480ae0f008f7f4e142
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-09 15:07:16 +0300

    Added CODE_CHANGED and onCodeChange.

--- client/scripts/arcademode/actions/ArcadeAction.js
+++ client/scripts/arcademode/actions/ArcadeAction.js
@@ -2,11 +2,20 @@
 'use strict';
 
 export const RUN_TEST = 'RUN_TEST';
+export const CODE_CHANGED = 'CODE_CHANGED';
 
 /* Action for when the runTest is clicked. */
-export function runTest () {
+export function runTest() {
   return {
     type: RUN_TEST,
     payload: 'test'
   };
 }
+
+export function onCodeChange(newCode) {
+  return {
+    type: CODE_CHANGED,
+    code: newCode
+  };
+}
+
