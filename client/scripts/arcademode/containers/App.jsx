50fe25b2c628b8ba5b384b1cec21e71ab8de00f5
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-11 00:17:58 +0300

    Added timeLeft to mapStateToProps and startTimer and stopTimer to mapDispatchToProps.

--- client/scripts/arcademode/containers/App.jsx
+++ client/scripts/arcademode/containers/App.jsx
@@ -4,19 +4,22 @@
 import { connect } from 'react-redux';
 
 import ArcadeMode from '../components/ArcadeMode';
-import { onCodeChange, runTests, startChallenge } from '../actions/ArcadeAction';
+import { onCodeChange, runTests, startChallenge, startTimer, stopTimer } from '../actions/ArcadeAction';
 
 const mapStateToProps = state => ({
   userData: state.arcadeReducer.userData,
   code: state.arcadeReducer.code,
   currChallenge: state.arcadeReducer.currChallenge,
-  testResults: state.arcadeReducer.testResults
+  testResults: state.arcadeReducer.testResults,
+  timeLeft: state.arcadeReducer.timeLeft
 });
 
 const mapDispatchToProps = dispatch => ({
   runTests: (userCode, currChallenge) => dispatch(runTests(userCode, currChallenge)),
   onCodeChange: newCode => dispatch(onCodeChange(newCode)),
-  startChallenge: () => dispatch(startChallenge())
+  startChallenge: () => dispatch(startChallenge()),
+  startTimer: () => dispatch(startTimer()),
+  stopTimer: () => dispatch(stopTimer())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ArcadeMode);
