0bb6a5a4441d194e8e5da4570cb8bab8465228f3
timolawl <timolawl@gmail.com>
2017-05-10 02:08:42 -0700

    clean up code

--- client/scripts/public/worker.js
+++ client/scripts/public/worker.js
@@ -10,38 +10,28 @@
 import * as Challenges from '../../json/challenges.json';
 
 onmessage = e => {
-
-
-  
-  // console.log(Challenges.challenges.map(challenge => challenge.challengeSeed));
-  // console.log(e.data);
-  
-  // const testCases = Challenges.challenges[0].tests;
-  // const testCondition = Challenges.challenges[0].tests.map(test => test.match(/^assert\(([^,]*),/)[1]);
-  // const testMessage = Challenges.challenges[0].tests.map(test => test.match(/message: (.*)'\);$/)[1]);
-
   const tests = Challenges.challenges[0].tests.map(test => {
     return {
-      test: test,
+      test,
       testCondition: test.match(/^assert\(([^,]*),/)[1],
-      testMessage: test.match(/message: (.*)'\);$/)[1] 
-    }
+      testMessage: test.match(/message: (.*)'\);$/)[1]
+    };
   });
 
   // Solution #1: Append 'return' and construct a new Function, then call that function.
   const reFn = e.data.replace(Challenges.challenges[0].challengeSeed[0],
-    'return ' + Challenges.challenges[0].challengeSeed[0]);
+    `return ${Challenges.challenges[0].challengeSeed[0]}`);
 
   const userFn = (new Function(reFn))();
   console.log(userFn('test string'));
 
   // single test case:
-  const testCase = assert + '\n' + e.data + '\n' + 'return ' + tests[0].test;
+  const testCase = `${assert};${e.data};return ${tests[0].test}`;
   const tc = (new Function(testCase))();
-  console.log('assert!: ' + tc);
+  console.log(`assert single test case: ${tc}`);
 
   // multiple test cases in one:
-  const reTestCases = tests.map(test => 
+  const reTestCases = tests.map(test =>
     `${assert};
     ${e.data};
    // console.log(${test.testCondition} ${test.testMessage});
@@ -49,9 +39,9 @@ onmessage = e => {
 
   const assertFns = reTestCases.map(reTestCase => (new Function(reTestCase))());
   // console.log('assert: ' + assertFns);
-  assertFns.map(assertFn => console.log('assert! ' + assertFn));
+  assertFns.map(assertFn => console.log(`assert all: ${assertFn}`));
 
-  // Solution: #2: Append the test case to the end the code below (since the last evaluation is returned)
+  // Solution: #2: Append the test case to the end the code below:
 
   // console.log('eval: ' + eval(testCase));
 
@@ -62,7 +52,6 @@ onmessage = e => {
 
   const userCode = eval((function () { return e.data; })());
   console.log(userCode); // this is the return for the user console box.
-  
 };
 
 // postMessage(runTestsAgainstUserCode());
