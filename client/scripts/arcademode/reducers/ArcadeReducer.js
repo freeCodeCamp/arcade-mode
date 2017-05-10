a4ce22540f93fb4df0a5d22b57b60cc4487c38f3
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-11 00:18:41 +0300

    Removed Timer class. Added handling of timer actions such as STOP_TIMER.

--- client/scripts/arcademode/reducers/ArcadeReducer.js
+++ client/scripts/arcademode/reducers/ArcadeReducer.js
@@ -5,15 +5,16 @@
 
 // import Interpreter from 'js-interpreter';
 
-import { TESTS_STARTED, CODE_CHANGED, START_CHALLENGE, TESTS_FINISHED, TIMER_FINISHED } from '../actions/ArcadeAction';
+import { TESTS_STARTED, CODE_CHANGED, START_CHALLENGE, TESTS_FINISHED, TIMER_STARTED, TIMER_UPDATED, STOP_TIMER } from '../actions/ArcadeAction';
 import UserData from '../model/UserData';
 import Challenges from '../../../json/challenges.json';
 import TestResults from '../model/TestResults';
 import Challenge from '../model/Challenge';
-import Timer from '../model/Timer';
 
 // const initialState = Immutable.Map();
 //
+
+const timerDefaultValue = 5 * 1000; // 15 minutes
 
 export default function arcadeReducer(state, action) {
   if (typeof state === 'undefined') {
@@ -24,12 +25,16 @@ export default function arcadeReducer(state, action) {
       userData: new UserData({ username: '' }),
       testResults: new TestResults([]),
       currChallenge: new Challenge(Challenges.challenges[0]),
+
+      // Timer handling
       isTimerFinished: false,
-      timer: new Timer(1000 * 900)
+      timerMaxValue: timerDefaultValue,
+      timeLeft: timerDefaultValue,
+      timerStart: 0
     };
   }
 
-  let nextState = Object.assign({}, state);
+  const nextState = Object.assign({}, state);
 
   switch (action.type) {
     case TESTS_STARTED: {
@@ -50,15 +55,23 @@ export default function arcadeReducer(state, action) {
       nextState.isTimerFinished = false;
       break;
     }
-    case TIMER_FINISHED: {
-      nextState.isTimerFinished = true;
+    case TIMER_STARTED: {
+      nextState.isTimerFinished = false;
+      nextState.timeLeft = timerDefaultValue;
+      nextState.timerStart = new Date().getTime();
       break;
     }
     case TIMER_UPDATED: {
-      nextState.timeLeft = state.timer.getTimeLeft();
+      const timeNow = new Date().getTime();
+      nextState.timeLeft = state.timerMaxValue - (timeNow - state.timerStart);
       break;
     }
-    default: console.log('Default reached.');
+    case STOP_TIMER: {
+      nextState.isTimerFinished = true;
+      nextState.timeLeft = 0;
+      break;
+    }
+    default: console.log('ERROR. ArcadeReducer default reached.');
   }
 
   return nextState;
