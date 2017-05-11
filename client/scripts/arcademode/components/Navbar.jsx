6600d100b9587f36d8f5e74236d85b1b76a0ba62
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-11 13:33:32 +0300

    Changed component style to functional.

--- client/scripts/arcademode/components/Navbar.jsx
+++ client/scripts/arcademode/components/Navbar.jsx
@@ -1,43 +1,44 @@
 
 'use strict';
 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Navbar, FormGroup, FormControl } from 'react-bootstrap';
 
-export default class ArcadeNavbar extends Component {
+/* Component at the top of page showing freeCodeCamp next and timer value. */
+const ArcadeNavbar = props => (
+  <Navbar fluid>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href='//freecodecamp.com'>freeCodeCamp Arcade Mode</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Navbar.Form pullLeft>
+        <FormGroup>
+          <FormControl
+            type='text' placeholder='Enter time in milliseconds here'
+            value={props.timerMaxValue}
+            onChange={props.onTimerMaxValueChange}
+          />
+        </FormGroup>
+      </Navbar.Form>
+      <Navbar.Text pullLeft>
+        Time: {props.timeLeft}
+      </Navbar.Text>
+      <Navbar.Text pullRight>
+        Signed in as: <Navbar.Link href='#'>Test</Navbar.Link>
+      </Navbar.Text>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
-  render() {
-    return (
-      <Navbar fluid>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href='//freecodecamp.com'>freeCodeCamp Arcade Mode</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Navbar.Form pullLeft>
-            <FormGroup>
-              <FormControl type='text' placeholder='Enter time in milliseconds here' value={this.props.timerMaxValue} onChange={this.props.onTimerMaxValueChange} />
-      {/* <input value={this.props.timerMaxValue} onChange={this.props.onTimerMaxValueChange} /> */}
-            </FormGroup>
-          </Navbar.Form>
-          <Navbar.Text pullLeft>
-            Time: {this.props.timeLeft}
-          </Navbar.Text>
-          <Navbar.Text pullRight>
-            Signed in as: <Navbar.Link href='#'>Test</Navbar.Link>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
-
-}
 
 ArcadeNavbar.propTypes = {
   timeLeft: PropTypes.number.isRequired,
   timerMaxValue: PropTypes.number.isRequired,
   onTimerMaxValueChange: PropTypes.func.isRequired
 };
+
+export default ArcadeNavbar;
