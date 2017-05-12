358b9b4fc631f836a498ca9a8b00f4e14f5f1ad4
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-12 23:28:21 +0300

    Added tests for initial state and starting a challenge.

--- test/client/scripts/arcademode/reducers/session.spec.js
+++ test/client/scripts/arcademode/reducers/session.spec.js
@@ -1,41 +1,41 @@
-// 
+//
 // 'use strict';
-// 
+//
 // // import Immutable from 'immutable';
-// 
+//
 // // import Interpreter from 'js-interpreter';
-// 
+//
 // import {
 //   FINISH_SESSION
 // } from '../actions/session';
-// 
+//
 // import { CHALLENGE_START, CHALLENGE_NEXT } from '../actions/challenge';
-// 
+//
 // import UserData from '../model/UserData';
-// 
+//
 // // const initialState = Immutable.Map();
-// 
+//
 // /* TODO: Returns score for completed challenge. */
 // const getScoreForChallenge = challenge => {
 //   return 1;
 // };
-// 
+//
 // export default function session (state, action) {
 //   if (typeof state === 'undefined') {
 //     return {
 //       // Challenges.challenges[0].challengeSeed.join('\n'),
 //       userOutput: 'The output of your code will show up here.',
 //       userData: new UserData({ username: '' }),
-// 
+//
 //       // Session control variables
 //       isSessionFinished: false,
 //       isSessionStarted: false,
 //       sessionScore: 0
 //     };
 //   }
-// 
+//
 //   const nextState = Object.assign({}, state);
-// 
+//
 //   switch (action.type) {
 //     case CHALLENGE_START:
 //       nextState.isSessionFinished = false;
@@ -53,20 +53,29 @@
 //       // console.log('ERROR. ArcadeReducer default reached.');
 //       return state;
 //   }
-// 
+//
 //   return nextState;
 // }
 
 /* Unit tests for file client/scripts/arcademode/reducers/session.js. */
 import { assert } from 'chai';
-import session from '../../../../..//client/scripts/arcademode/reducers/session.js'
+import reducer from '../../../../..//client/scripts/arcademode/reducers/session';
+import { startChallenge } from '../../../../../client/scripts/arcademode/actions/challenge';
 
+const dummyAction = { type: 'DUMMY' };
 
-describe('session', () => {
-
-  it('should do x', () => {
-    assert(/* code */);
+describe('session reducer', () => {
+  it('should indicate that no session has been started or finished', () => {
+    const initialState = reducer(undefined, dummyAction);
+    assert(initialState.isSessionStarted === false);
+    assert(initialState.isSessionFinished === false);
   });
 
+  it('should start the session with an action', () => {
+    const initialState = reducer(undefined, dummyAction);
+    const nextState = reducer(initialState, startChallenge(0));
+    assert(initialState.isSessionStarted === false);
+    assert(nextState.isSessionStarted === true);
+  });
 });
 
