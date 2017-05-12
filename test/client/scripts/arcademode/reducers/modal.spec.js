de4304c57ff8aff1c7227aee09a60e3d793285e1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-12 20:11:30 +0300

    Removed the commented code.

--- test/client/scripts/arcademode/reducers/modal.spec.js
+++ test/client/scripts/arcademode/reducers/modal.spec.js
@@ -1,27 +1,5 @@
-//
-// 'use strict';
-//
-// import { MODAL_CLOSE } from '../actions/modal';
-//
-// // import Immutable from 'immutable';
-//
-// const initialState = {
-//   modal: true // display initially
-// };
-//
-// export default function modal(state = initialState, action) {
-//   const nextState = Object.assign({}, state);
-//
-//   switch (action.type) {
-//     case MODAL_CLOSE:
-//       nextState.modal = false;
-//       break;
-//     default:
-//       return state;
-//   }
-//
-//   return nextState;
-// }
+
+'use strict';
 
 /* Unit tests for file client/scripts/arcademode/reducers/modal.js. */
 import { assert } from 'chai';
