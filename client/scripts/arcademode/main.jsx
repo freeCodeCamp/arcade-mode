d3eea752f8e8421988c3ab275b241fad74242d8b
timolawl <timolawl@gmail.com>
2017-05-11 17:18:34 -0700

    store to own dir; combine reducers in index

--- client/scripts/arcademode/main.jsx
+++ client/scripts/arcademode/main.jsx
@@ -3,34 +3,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import ThunkMiddleware from 'redux-thunk';
-// import { combineReducers } from 'redux-immutable';
+
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 import App from './containers/App';
-import arcadeReducer from './reducers/ArcadeReducer';
-import composeEnhancers from './reducers/composeEnhancers';
 
-const ReactRedux = require('react-redux');
-
-const Provider = ReactRedux.Provider;
-
+const store = configureStore();
 const app = document.querySelector('.app');
 
-const reducersCombined = combineReducers({
-  arcadeReducer
-});
-
-const mainStore = createStore(reducersCombined,
-  composeEnhancers(
-    applyMiddleware(
-        ThunkMiddleware // Lets us dispatch functions
-    )
-));
-
-
 ReactDOM.render(
-  <Provider store={mainStore}>
+  <Provider store={store}>
     <App />
   </Provider>, app
 );
