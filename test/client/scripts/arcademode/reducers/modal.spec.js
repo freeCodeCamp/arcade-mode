91085caaf12c418b817cf24d4691cb380f734ada
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-12 20:08:09 +0300

    Added test for MODAL_CLOSE.

--- test/client/scripts/arcademode/reducers/modal.spec.js
+++ test/client/scripts/arcademode/reducers/modal.spec.js
@@ -1,17 +1,17 @@
-// 
+//
 // 'use strict';
-// 
+//
 // import { MODAL_CLOSE } from '../actions/modal';
-// 
+//
 // // import Immutable from 'immutable';
-// 
+//
 // const initialState = {
 //   modal: true // display initially
 // };
-// 
+//
 // export default function modal(state = initialState, action) {
 //   const nextState = Object.assign({}, state);
-// 
+//
 //   switch (action.type) {
 //     case MODAL_CLOSE:
 //       nextState.modal = false;
@@ -19,20 +19,24 @@
 //     default:
 //       return state;
 //   }
-// 
+//
 //   return nextState;
 // }
 
 /* Unit tests for file client/scripts/arcademode/reducers/modal.js. */
 import { assert } from 'chai';
-import modal from '../../../../..//client/scripts/arcademode/reducers/modal.js'
+import reducer from '../../../../../client/scripts/arcademode/reducers/modal';
 
+import { onModalClose } from '../../../../../client/scripts/arcademode/actions/modal';
 
-describe('modal', () => {
+const dummyAction = { type: 'DUMMY' };
 
-  it('should do x', () => {
-    assert(/* code */);
+describe('modal reducer', () => {
+  it('should do close modal upon MODAL_CLOSE', () => {
+    const state = reducer(undefined, dummyAction);
+    assert(state.modal === true, 'Modal is open OK');
+    const nextState = reducer(state, onModalClose());
+    assert(nextState.modal === false, 'Modal is closed OK');
   });
-
 });
 
