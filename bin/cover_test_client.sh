7b67f1dafddc522b07eb3e50431102e3e01295f7
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-12 14:39:16 +0300

    Produces now HTML code coverage report.

--- bin/cover_test_client.sh
+++ bin/cover_test_client.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 node_modules/.bin/babel-node node_modules/.bin/babel-istanbul cover node_modules/.bin/_mocha -- --recursive test/client
+istanbul report --root coverage html --dir coverage_html_client
+
