0ad51d533c5ddd72f6f055213cfbf35145c9a241
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-13 10:29:28 +0300

    Removed the commented out original code from the test-files.

--- test/client/scripts/arcademode/reducers/test.spec.js
+++ test/client/scripts/arcademode/reducers/test.spec.js
@@ -1,44 +1,5 @@
-//
-// 'use strict';
-//
-// import { OUTPUT_CHANGED, TESTS_STARTED, TESTS_FINISHED } from '../actions/test';
-// import { CHALLENGE_NEXT } from '../actions/challenge';
-// import { TIMER_FINISHED } from '../actions/timer';
-// import TestResults from '../model/TestResults';
-//
-// const initialState = {
-//   userOutput: 'The output of your code will show up here.',
-//   isRunningTests: false,
-//   testResults: new TestResults([])
-// };
-//
-// export default function test (state = initialState, action) {
-//   const nextState = Object.assign({}, state);
-//
-//   switch (action.type) {
-//     case CHALLENGE_NEXT:
-//       nextState.userOutput = 'The output of your code will show up here.';
-//       nextState.testResults = new TestResults([]);
-//       break;
-//     case TIMER_FINISHED:
-//       nextState.testResults = new TestResults(action.testResults);
-//       break;
-//     case OUTPUT_CHANGED:
-//       nextState.userOutput = action.userOutput;
-//       break;
-//     case TESTS_STARTED:
-//       nextState.isRunningTests = true;
-//       break;
-//     case TESTS_FINISHED:
-//       nextState.isRunningTests = false;
-//       nextState.testResults = new TestResults(action.testResults);
-//       break;
-//     default:
-//       return state;
-//   }
-//
-//   return nextState;
-// }
+
+'use strict';
 
 /* Unit tests for file client/scripts/arcademode/reducers/test.js. */
 import chai, { expect } from 'chai';
--- test/client/scripts/arcademode/reducers/timer.spec.js
+++ test/client/scripts/arcademode/reducers/timer.spec.js
@@ -1,53 +1,5 @@
-// 
-// 'use strict';
-// 
-// import {
-//   STOP_TIMER,
-//   TIMER_STARTED,
-//   TIMER_UPDATED,
-//   TIMER_FINISHED,
-//   TIMER_MAX_VALUE_CHANGED
-// } from '../actions/timer';
-// 
-// const timerDefaultValue = 60 * 1000;
-// 
-// const initialState = {
-//   isTimerFinished: false,
-//   timerMaxValue: timerDefaultValue,
-//   timerMaxValueLoaded: timerDefaultValue,
-//   timeLeft: timerDefaultValue,
-//   timerStart: 0
-// };
-// 
-// export default function timer (state = initialState, action) {
-//   const nextState = Object.assign({}, state);
-// 
-//   switch (action.type) {
-//     case STOP_TIMER:
-//       nextState.isTimerFinished = true;
-//       nextState.timeLeft = 0;
-//       break;
-//     case TIMER_STARTED:
-//       nextState.isTimerFinished = false;
-//       nextState.timeLeft = timerDefaultValue;
-//       nextState.timerStart = action.startTime;
-//       break;
-//     case TIMER_UPDATED:
-//       const timeNow = action.timeNow;
-//       nextState.timeLeft = parseInt(state.timerMaxValueLoaded, 10) - (timeNow - state.timerStart);
-//       break;
-//     case TIMER_FINISHED:
-//       nextState.isRunningTests = false;
-//       break;
-//     case TIMER_MAX_VALUE_CHANGED:
-//       nextState.timerMaxValue = action.timerMaxValue;
-//       break;
-//     default:
-//       return state;
-//   }
-// 
-//   return nextState;
-// }
+
+'use strict';
 
 /* Unit tests for file client/scripts/arcademode/reducers/timer.js. */
 import chai, { expect } from 'chai';
@@ -112,13 +64,13 @@ describe('timer reducer', () => {
     }));
   });
 
-  /* 
+  /*
   // currently no TIMER_FINISHED action; see STOP_TIMER?
   it('should finish timer on TIMER_FINISHED', () => {
     const state = Immutable.Map({
       isRunningTests: true
     });
-    const nextState = reducer(state, 
+    const nextState = reducer(state,
   });
   */
 
