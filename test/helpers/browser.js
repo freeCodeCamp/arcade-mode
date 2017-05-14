2699047f2e73df82e5c628598d988d1deb893a43
timolawl <timolawl@gmail.com>
2017-05-14 15:36:36 -0700

    fix and cleanup jsdom setup

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -9,22 +9,15 @@
 import { JSDOM } from 'jsdom';
 
 const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
-// const win = doc.defaultView;
-const exposedProperties = ['window', 'navigator', 'document'];
 
 global.window = dom.window;
 global.document = dom.window.document;
-// global.document = doc;
-// global.window = win;
 
 Object.keys(global.window).forEach(property => {
   if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
     global[property] = global.window[property];
   }
 });
-
-global.Worker = global.window.Worker;
 
 global.navigator = {
   userAgent: 'node.js'
