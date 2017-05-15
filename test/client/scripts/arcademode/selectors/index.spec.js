4b0d630c1aeae7dd3fa782f88c18e7144f287564
timolawl <timolawl@gmail.com>
2017-05-14 20:43:29 -0700

    initial commit

---
+++ test/client/scripts/arcademode/selectors/index.spec.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+import { expect } from 'chai';
+
+import selector from '../../../../../client/scripts/arcademode/selectors';
+
+describe('Selector: index.js', () => {
+  it('should ...', () => {
+    
+  });
+});
