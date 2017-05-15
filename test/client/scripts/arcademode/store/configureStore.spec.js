a9d67c9f29270b56be21cb71073020f8957374d4
timolawl <timolawl@gmail.com>
2017-05-14 20:42:07 -0700

    Test store, dispatch, and state

--- test/client/scripts/arcademode/store/configureStore.spec.js
+++ test/client/scripts/arcademode/store/configureStore.spec.js
@@ -5,17 +5,18 @@
 import { expect } from 'chai';
 import configureStore from '../../../../../client/scripts/arcademode/store/configureStore';
 
-describe('configureStore()', () => {
-  it('should do return an object', () => {
+describe('Store: configureStore()', () => {
+  it('should return an object representing the store', () => {
     const store = configureStore();
     expect(typeof store).to.equal('object');
     const state = store.getState();
-    expect(state).not.to.empty;
+    expect(state).not.to.be.empty;
   });
 
-  it('should do accept dispatched actions', () => {
+  it('should accept dispatched actions and update its state', () => {
     const store = configureStore();
-    store.dispatch({ type: 'DUMMY' });
+    store.dispatch({ type: 'MODAL_CLOSE' });
+    expect(store.getState().getIn(['modal', 'modal'])).to.be.false;
   });
 });
 
