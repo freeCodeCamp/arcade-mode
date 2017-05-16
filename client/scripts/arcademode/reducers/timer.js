066b34d51d2870e4c69bdea53b885b5aa9b1ba77
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-16 19:57:00 +0200

    Added conversion of timerMaxValue to string.

--- client/scripts/arcademode/reducers/timer.js
+++ client/scripts/arcademode/reducers/timer.js
@@ -17,7 +17,7 @@ const timerDefaultValue = 60 * 1000;
 
 const initialState = Immutable.Map({
   isTimerFinished: false,
-  timerMaxValue: timerDefaultValue,
+  timerMaxValue: `${timerDefaultValue}`,
   timerMaxValueLoaded: timerDefaultValue,
   timeLeft: timerDefaultValue,
   timerStart: 0
