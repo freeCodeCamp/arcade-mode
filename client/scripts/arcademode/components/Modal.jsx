5c9a06825c2fde057c51af6c7265481b526001e7
timolawl <timolawl@gmail.com>
2017-05-17 19:54:00 -0700

    update modal with options; clean up stylesheets

--- client/scripts/arcademode/components/Modal.jsx
+++ client/scripts/arcademode/components/Modal.jsx
@@ -4,22 +4,65 @@
 import React from 'react';
 // import ImmutablePropTypes from 'react-immutable-proptypes';
 import PropTypes from 'prop-types';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Col, Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
 const ArcadeModal = props => (
-  <Modal show={props.modal} onHide={props.onModalClose}>
+  <Modal show={props.modal} onHide={props.onModalClose} animation={false} backdrop={'static'}>
     <Modal.Header closeButton>
-      <Modal.Title>{" Welcome to freeCodeCamp's Arcade Mode!"}</Modal.Title>
+      <Modal.Title bsClass='am__modal__title'>{'freeCodeCamp\'s Arcade Mode'}</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <h4>Quick start:</h4>
-      <p>This is an app where you can practice your algorithm and data structure
-        skills by testing them in a timed environment.</p>
-      <p>All you have to do is set the time and press start, and off you go!</p>
+  { /* <h4>Quick start:</h4> */ }
+      <p>Welcome to freeCodeCamp's Arcade Mode.
+        This is a place where you can practice your algorithm and data structure skills.</p>
+      <br />
+      <Form horizontal>
+        <FormGroup>
+          <Col sm={3}>
+            <ControlLabel>Mode:</ControlLabel>
+          </Col>
+          <Col smOffset={3} sm={6}>
+            <FormControl componentClass='select' placeholder='Mode'>
+              <option value='Arcade'>Arcade</option>
+              <option value='Practice'>Practice</option>
+            </FormControl>
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col sm={3}>
+            <ControlLabel>Difficulty:</ControlLabel>
+          </Col>
+          <Col smOffset={3} sm={6}>
+            <FormControl componentClass='select' placeholder='Difficulty'>
+              <option value='Easy'>Easy</option>
+              <option value='Medium' selected>Medium</option>
+              <option value='Hard'>Hard</option>
+              <option value='Random'>Random</option>
+            </FormControl>
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col sm={3}>
+            <ControlLabel>Editor Setting:</ControlLabel>
+          </Col>
+          <Col smOffset={3} sm={6}>
+            <FormControl componentClass='select' placeholder='Editor Setting'>
+              <option value='Normal'>Normal</option>
+              <option value='Whiteboard'>Whiteboard</option>
+            </FormControl>
+          </Col>
+        </FormGroup>
+        <br />
+        <FormGroup bsClass='am__modal__submit'>
+          <Button type='submit' onClick={props.onModalClose}>Start</Button>
+        </FormGroup>
+      </Form>
     </Modal.Body>
+    {/*
     <Modal.Footer>
-      <Button onClick={props.onModalClose}>Close</Button>
+      <Button type='submit' onClick={props.onModalClose}>Start</Button>
     </Modal.Footer>
+    */}
   </Modal>
 );
 
