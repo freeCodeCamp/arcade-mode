6b0b79f4814047182e23de29c70bd80b85fb2c1e
timolawl <timolawl@gmail.com>
2017-05-18 01:15:14 -0700

    add gamesettings, playerstatus

--- client/scripts/arcademode/reducers/index.js
+++ client/scripts/arcademode/reducers/index.js
@@ -3,16 +3,20 @@
 
 import { combineReducers } from 'redux-immutable';
 
-import session from './session';
 import challenge from './challenge';
+import gamesettings from './gamesettings';
 import modal from './modal';
+import playerstatus from './playerstatus';
+import session from './session';
 import test from './test';
 import timer from './timer';
 
 export default combineReducers({
-  session,
   challenge,
+  gamesettings,
   modal,
+  playerstatus,
+  session,
   test,
   timer
 });
