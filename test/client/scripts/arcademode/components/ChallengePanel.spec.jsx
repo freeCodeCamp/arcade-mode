928e0db23f1fbe1a22519db1cb2b2ffe13a2083f
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-19 20:41:47 +0200

    Added unit tests for ChallengePanel.

---
+++ test/client/scripts/arcademode/components/ChallengePanel.spec.jsx
@@ -0,0 +1,36 @@
+
+/* Unit tests for file client/scripts/arcademode/components/ArcadeMode.jsx. */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import chaiEnzyme from 'chai-enzyme';
+import chai, { expect } from 'chai';
+
+import Immutable from 'immutable';
+import CodeMirror from 'react-codemirror';
+import ChallengePanel from '../../../../../client/scripts/arcademode/components/ChallengePanel';
+
+chai.use(chaiEnzyme());
+
+const props = {
+  testResults: Immutable.List(),
+  description: Immutable.List(['a', 'b', 'c'])
+};
+
+describe('<ChallengePanel> component', () => {
+  it('should render', () => {
+    const wrapper = shallow(<ChallengePanel {...props} />);
+    expect(wrapper).to.have.length(1);
+  });
+
+  it('should one <CodeMirror>', () => {
+    const wrapper = shallow(<ChallengePanel {...props} />);
+    expect(wrapper.find(CodeMirror)).to.have.length(1);
+  });
+
+  it('should have start <button>', () => {
+    const wrapper = shallow(<ChallengePanel {...props} />);
+    const btnDiv = wrapper.find('.challenge__buttons');
+    expect(btnDiv.find('button').first()).to.include.text('Start');
+  });
+});
