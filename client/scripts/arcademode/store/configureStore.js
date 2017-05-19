524902f205daef07a12960b5b2d261182a5be544
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-19 08:26:26 +0200

    Added redux-invariant for the store.

--- client/scripts/arcademode/store/configureStore.js
+++ client/scripts/arcademode/store/configureStore.js
@@ -3,6 +3,8 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import invariant from 'redux-immutable-state-invariant';
+
 import appReducer from '../reducers';
 import composeEnhancers from '../reducers/composeEnhancers';
 // import Immutable from 'immutable';
@@ -10,6 +12,6 @@ import composeEnhancers from '../reducers/composeEnhancers';
 
 export default function configureStore () {
   return createStore(appReducer, composeEnhancers(applyMiddleware(
-    thunk
+    invariant(), thunk
   )));
 }
