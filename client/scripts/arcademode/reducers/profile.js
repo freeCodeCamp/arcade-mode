ec579ac87b8f399e83fef2ca73af921b6483c939
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-20 18:38:49 +0200

    Added UPDATE_USER_DATA.

--- client/scripts/arcademode/reducers/profile.js
+++ client/scripts/arcademode/reducers/profile.js
@@ -6,7 +6,8 @@ import Immutable from 'immutable';
 import {
   SHOW_PROFILE,
   HIDE_PROFILE,
-  LOAD_USER_DATA
+  LOAD_USER_DATA,
+  UPDATE_USER_DATA
 } from '../actions/profile';
 
 import UserData from '../models/UserData';
@@ -24,6 +25,10 @@ export default function profile(state = initialState, action) {
       .set('isProfileShown', true);
     case LOAD_USER_DATA: return state
       .set('userData', action.userData);
+    case UPDATE_USER_DATA: return state
+      .update('userData', userData =>
+        userData.appendSession(action.session)
+      );
     default:
       return state;
   }
