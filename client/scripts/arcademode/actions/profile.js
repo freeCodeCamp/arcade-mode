b6d1b42f07419b0017196e49614a9d71edf2f7cf
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-20 18:31:08 +0200

    Added actionUpdateUserData().

--- client/scripts/arcademode/actions/profile.js
+++ client/scripts/arcademode/actions/profile.js
@@ -2,6 +2,7 @@
 export const SHOW_PROFILE = 'SHOW_PROFILE';
 export const HIDE_PROFILE = 'HIDE_PROFILE';
 export const LOAD_USER_DATA = 'LOAD_USER_DATA';
+export const UPDATE_USER_DATA = 'UPDATE_USER_DATA';
 
 export function actionShowProfile() {
   return {
@@ -19,5 +20,12 @@ export function actionLoadUserData(userData) {
   return {
     type: LOAD_USER_DATA,
     userData
+  };
+}
+
+export function actionUpdateUserData(session) {
+  return {
+    type: UPDATE_USER_DATA,
+    session
   };
 }
