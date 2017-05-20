cddc7288cefe065c40be18905950620efac92405
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-20 18:36:24 +0200

    Added renderig of sessions and challenges.

--- client/scripts/arcademode/components/UserProfile.jsx
+++ client/scripts/arcademode/components/UserProfile.jsx
@@ -7,25 +7,49 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 import UserData from '../models/UserData';
 
-function renderChallenges(challenges) {
-  const challengesJsx = challenges.map(challenge => {
-    return (
-      <ListGroupItem key={challenge.get('id')}>
-        Challenge {challenge.get('id')}
-      </ListGroupItem>
-    );
-  });
-  return challengesJsx;
-}
 
 export default class UserProfile extends Component {
 
+  constructor(props) {
+    super(props);
+    this.expandChallengeView = this.expandChallengeView.bind(this);
+    this.expandSessionView = this.expandSessionView.bind(this);
+  }
+
+  expandChallengeView(sessionId, challengeId) {
+    console.log(`expandChallengeView ${sessionId} ${challengeId}`);
+  }
+
+  expandSessionView(sessionId) {
+    console.log(`expandSessionView ${sessionId}`);
+  }
+
+  // TODO: Refactor into own component ProfileChallenge to avoid bind
+  renderChallenges(sessionId, challenges) {
+    const challengesJsx = challenges.map(challenge => {
+      return (
+        <ListGroupItem
+          key={challenge.id}
+          onClick={this.expandChallengeView.bind(this, sessionId, challenge.id)}
+        >
+          Challenge {challenge.id}
+        </ListGroupItem>
+      );
+    });
+    return challengesJsx;
+  }
+
   renderSessions() {
     const sessions = this.props.userData.sessions.map(session => {
-      const challenges = renderChallenges(session.get('challenges'));
+      const sessionId = session.get('id');
+      const challenges = this.renderChallenges(sessionId, session.get('challenges'));
+      // TODO: Refactor into own component ProfileSession to avoid bind
       return (
-        <ListGroupItem key={session.get('id')} >
-          Session: {session.get('id') }
+        <ListGroupItem
+          key={sessionId}
+        >
+          Session: {sessionId}
+          <button onClick={this.expandSessionView.bind(this, sessionId)}>Expand</button>
           <ListGroup>
             {challenges}
           </ListGroup>
@@ -40,6 +64,9 @@ export default class UserProfile extends Component {
     return (
       <div className='user-profile'>
         <h1>User Profile</h1>
+        <p>You can view your past sessions here. By clicking on a session, you can expand it
+          to view all challenges completed during that session.
+        </p>
         <div className='profile-data'>
           <ListGroup>
             {sessions}
