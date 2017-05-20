2f6d205db692b98bdc42647888d9178128b4f9be
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-20 15:22:58 +0200

    Added reducers for user profile management.

---
+++ client/scripts/arcademode/reducers/profile.js
@@ -0,0 +1,30 @@
+
+'use strict';
+
+import Immutable from 'immutable';
+
+import {
+  SHOW_PROFILE,
+  HIDE_PROFILE,
+  LOAD_USER_DATA
+} from '../actions/profile';
+
+import UserData from '../models/UserData';
+
+const initialState = Immutable.Map({
+  isProfileShown: false,
+  userData: new UserData()
+});
+
+export default function profile(state = initialState, action) {
+  switch (action.type) {
+    case HIDE_PROFILE: return state
+      .set('isProfileShown', false);
+    case SHOW_PROFILE: return state
+      .set('isProfileShown', true);
+    case LOAD_USER_DATA: return state
+      .set('userData', action.userData);
+    default:
+      return state;
+  }
+}
