5d922e4b287117f69b802ff2165e3cc8d4770ca2
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-21 17:08:52 +0200

    Removed logging.

--- client/scripts/arcademode/reducers/playerstatus.js
+++ client/scripts/arcademode/reducers/playerstatus.js
@@ -35,7 +35,6 @@ const difficultySettings = {
 };
 
 export default function playerstatus (state = initialState, action) {
-  console.log('playerstatus reducer: ' + action.type);
   switch (action.type) {
     case CHALLENGE_START:
       return state
