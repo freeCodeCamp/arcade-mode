c62c99bca86e87577cd9fb29f4f7ea2d0fc3e1f8
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-21 17:10:24 +0200

    Added default props. Started sub-componen test.

--- test/client/scripts/arcademode/components/UserProfile.spec.jsx
+++ test/client/scripts/arcademode/components/UserProfile.spec.jsx
@@ -4,19 +4,32 @@ import { shallow } from 'enzyme';
 
 import chaiEnzyme from 'chai-enzyme';
 import chai, { expect } from 'chai';
-
-// import Immutable from 'immutable';
-
-// import { Grid, Row, Col } from 'react-bootstrap';
+import sinon from 'sinon';
+import { ListGroup } from 'react-bootstrap';
 
 import UserProfile from '../../../../../client/scripts/arcademode/components/UserProfile';
+import UserData from '../../../../../client/scripts/arcademode/models/UserData';
+
 
 chai.use(chaiEnzyme());
 
-describe('<UserProfile> component', () => {
+const deleteSession = sinon.spy();
+
+const props = {
+  userData: new UserData(),
+  deleteSession
+};
+
+describe('Component: <UserProfile>', () => {
   it('should render without errors', () => {
-    const wrapper = shallow(<UserProfile />);
+    const wrapper = shallow(<UserProfile {...props} />);
     expect(wrapper).to.have.length(1);
+  });
+
+  it('should render all subcomponents', () => {
+    const wrapper = shallow(<UserProfile {...props} />);
+    expect(wrapper.find('h1')).to.include.text('User Profile');
+    expect(wrapper.find(ListGroup)).to.have.length(1);
   });
 });
 
