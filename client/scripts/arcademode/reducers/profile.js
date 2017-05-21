06c8e09c8484647a3fdccf6c5c27836e80b5590e
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-21 14:05:03 +0200

    Added handling of UPDATE_USER_DATA and DELETE_SESSION.

--- client/scripts/arcademode/reducers/profile.js
+++ client/scripts/arcademode/reducers/profile.js
@@ -7,7 +7,8 @@ import {
   SHOW_PROFILE,
   HIDE_PROFILE,
   LOAD_USER_DATA,
-  UPDATE_USER_DATA
+  UPDATE_USER_DATA,
+  DELETE_SESSION
 } from '../actions/profile';
 
 import UserData from '../models/UserData';
@@ -19,16 +20,25 @@ const initialState = Immutable.Map({
 
 export default function profile(state = initialState, action) {
   switch (action.type) {
-    case HIDE_PROFILE: return state
-      .set('isProfileShown', false);
-    case SHOW_PROFILE: return state
-      .set('isProfileShown', true);
-    case LOAD_USER_DATA: return state
-      .set('userData', action.userData);
-    case UPDATE_USER_DATA: return state
-      .update('userData', userData =>
-        userData.appendSession(action.session)
-      );
+    case HIDE_PROFILE:
+      return state
+        .set('isProfileShown', false);
+    case SHOW_PROFILE:
+      return state
+        .set('isProfileShown', true);
+    case LOAD_USER_DATA:
+      return state
+        .set('userData', new UserData(action.userData));
+    case UPDATE_USER_DATA:
+      return state
+        .update('userData', userData =>
+          userData.appendSession(action.session)
+        );
+    case DELETE_SESSION:
+      return state
+        .update('userData', userData =>
+          userData.deleteSession(action.sessionId)
+        );
     default:
       return state;
   }
