82ca7b25b3a13720a8583a7fb8663ac095bb8568
timolawl <timolawl@gmail.com>
2017-05-21 21:29:21 -0700

    move title canvas to own file

--- client/scripts/public/arcademode.js
+++ client/scripts/public/arcademode.js
@@ -1,56 +1,8 @@
 
 'use strict';
 
+import titleCanvas from './titleCanvas';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const $ = {};
-
-  $.init = () => {
-    $.canvas = document.querySelector('.am__am__canvas');
-    $.ctx = $.canvas.getContext('2d');
-    $.mouse = {
-      over: 0
-    };
-
-    $.reset();
-    $.loop();
-  };
-
-  $.reset = () => {
-    $.width = 220;
-    $.height = 50;
-    $.hue = 0;
-  };
-  $.step = () => {
-    $.hue < 360 ? $.hue++ : $.hue = 0;
-  };
-
-  $.draw = () => {
-    let pw = $.width;
-    while (pw--) {
-      if ($.mouse.over) {
-        $.ctx.fillStyle = `hsla(${$.hue - pw}, 100%, 85%, 0.5)`;
-      }
-      else $.ctx.fillStyle = `hsla(${$.hue - pw}, 100%, 90%, 0.5)`;
-      $.ctx.fillRect(pw, 0, 1, 50);
-    }
-  };
-
-  $.loop = () => {
-    requestAnimationFrame($.loop);
-    $.step();
-    $.draw();
-  };
-
-  $.init();
-
-  $.mouseover = () => {
-    $.mouse.over = 1;
-  };
-
-  $.mouseout = () => {
-    $.mouse.over = 0;
-  };
-
-  document.querySelector('.am__am__link').addEventListener('mouseover', $.mouseover);
-  document.querySelector('.am__am__link').addEventListener('mouseout', $.mouseout);
+  titleCanvas().init();
 });
