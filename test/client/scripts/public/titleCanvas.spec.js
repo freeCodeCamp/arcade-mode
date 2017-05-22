c1ef70044cda85e1af3d5255d63c4fb3241722e4
timolawl <timolawl@gmail.com>
2017-05-21 21:33:42 -0700

    add unit test for canvas el

---
+++ test/client/scripts/public/titleCanvas.spec.js
@@ -0,0 +1,28 @@
+
+'use strict';
+
+import { expect } from 'chai';
+
+import titleCanvas from '../../../../client/scripts/public/titleCanvas';
+
+describe('ARCADE MODE Canvas animation', () => {
+  const canvasObject = titleCanvas();
+
+  before(() => {
+    document.body.innerHTML =
+'<a href="//arcademode.herokuapp.com" class="am__am__link"><div class="am__am__logo"><canvas class="am__am__canvas" height="50" width="220">ARCADE MODE</canvas><svg class="am__am__svg"><clipPath id="arcadePath"><text class="am__am__canvas__text" x="0" y="35">ARCADE MODE</text></clipPath></svg></div></a>';
+
+    canvasObject.init();
+  });
+
+  after(() => {
+    document.body.removeChild(document.querySelector('.am__am__link'));
+  });
+
+  it('should render', () => {
+    const canvasEl = document.querySelector('.am__am__canvas');
+    expect(canvasEl.height).to.equal(50);
+    expect(canvasEl.width).to.equal(220);
+    expect(canvasObject.hue).to.be.within(0, 360);
+  });
+});
