aad53f3021048f26be6c649ec8895f4442233543
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-24 20:01:39 +0200

    Added unit tests for profile reducer.

--- test/client/scripts/arcademode/reducers/profile.spec.js
+++ test/client/scripts/arcademode/reducers/profile.spec.js
@@ -9,6 +9,10 @@ import profile, {
   createExpandStatus
 } from '../../../../../client/scripts/arcademode/reducers/profile';
 
+import {
+  actionHideProfile, actionShowProfile
+} from '../../../../../client/scripts/arcademode/actions/profile';
+
 chai.use(chaiImmutable);
 
 describe('Reducer: profile', () => {
@@ -27,6 +31,14 @@ describe('Reducer: profile', () => {
     it('should return well-defined initial state', () => {
       const initialState = profile(undefined, { type: 'DUMMY' });
       expect(initialState.get('isProfileShown')).to.equal(false);
+    });
+
+    it('should show/hide profile on action', () => {
+      const initialState = profile(undefined, { type: 'DUMMY' });
+      const showState = profile(initialState, actionShowProfile());
+      expect(showState.get('isProfileShown')).to.equal(true);
+      const hideState = profile(initialState, actionHideProfile());
+      expect(hideState.get('isProfileShown')).to.equal(false);
     });
   });
 });
