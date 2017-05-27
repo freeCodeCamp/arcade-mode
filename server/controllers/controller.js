59d981af83fdbfe1e68a2c8aec9b73a8b26582ce
timolawl <timolawl@gmail.com>
2017-05-27 15:30:18 -0700

    remove offline variable

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,12 +1,7 @@
 
-let offline = false;
-if (process.env.ARCADE_MODE_OFFLINE) {
-  offline = true;
-}
-
 function controller () {
   this.getIndex = (req, res) => {
-    res.render('index', { offline });
+    res.render('index');
   };
 }
 
