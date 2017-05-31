637c7c98d60293df8fb9d7410662267d375cfd5a
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-05-31 23:38:58 +0300

    Added challenges folder and one example challenge.

---
+++ bin/js2json_challenges.js
@@ -0,0 +1,155 @@
+#! /usr/bin/env node
+
+/* A script for converting javascript challenge code into JSON.
+  * Each challenge should have sections marked with comments '/// keyword:',
+  * For example:
+  *   /// solution:
+  *   function MySolution()...
+  *
+  *   /// tests:
+  *   assert(MySolution(2) === 3)...
+  *
+  * */
+
+const commandLineArgs = require('command-line-args');
+const fs = require('fs');
+const acorn = require('acorn');
+
+const optionDefinitions = [
+  { name: 'force', type: Boolean, descr: 'Overwrite output files forcefully.' },
+  { name: 'help', alias: 'h', type: Boolean, descr: 'Print help message' },
+  { name: 'infile', alias: 'f', type: String, multiple: true, descr: 'Input files' },
+  { name: 'outfile', alias: 'o', type: String, descr: 'Output JSON file' },
+  { name: 'verbose', alias: 'v', type: Boolean, descr: 'Run in verbose mode' }
+];
+
+// Regexp definitions for the script
+const re = {
+  lineComment: /^\/\/\/\s+(\w+):/
+};
+
+const opts = commandLineArgs(optionDefinitions);
+
+if (opts.help) {
+  usage(0);
+}
+
+const expectedProps = {
+  description: String,
+  challengeSeed: 'Code',
+  solution: 'Code',
+  tests: 'Code'
+};
+
+const files = process.argv.splice(2);
+if (opts.infile) {
+  files.concat(opts.infile);
+}
+
+if (files.length === 0) {
+  console.error('Error. No input files were given.');
+  usage(1);
+}
+
+const propParser = {
+  state: 'IDLE',
+  currFile: null,
+  prop: null,
+  propValue: '',
+  files: {}
+};
+
+files.forEach(file => {
+  processFile(propParser, file, expectedProps);
+});
+
+const result = formatResult(propParser.files);
+
+printToOutput(result);
+
+//---------------------------------------------------------------------------
+// HELPER FUNCTIONS
+//---------------------------------------------------------------------------
+
+function processFile(parser, file, props) {
+  const buffer = fs.readFileSync(file);
+  parser.currFile = file;
+  parser.files[file] = {};
+
+  try {
+    acorn.parse(buffer);
+  }
+  catch (err) {
+    console.error(`Parsing failed: ${err}`);
+    process.exit(1);
+  }
+
+  const lines = buffer.toString().split('\n');
+  lines.forEach(line => {
+    processLine(parser, line);
+  });
+
+  if (parser.propValue !== '') {
+    parser.files[parser.currFile][parser.prop] = parser.propValue;
+    parser.propValue = null;
+  }
+
+  verifyExpectedProps(parser, file, props);
+}
+
+/* Processes one line with regex and extracts prop values. */
+function processLine(parser, line) {
+  if (re.lineComment.test(line)) {
+    const matches = line.match(re.lineComment);
+    if (parser.prop !== null) {
+      parser.files[parser.currFile][parser.prop] = parser.propValue;
+      parser.propValue = '';
+    }
+    parser.prop = matches[1];
+  }
+  else if (parser.prop !== null) {
+    parser.propValue += line;
+  }
+}
+
+/* Verifies that the parsed file contains expected props like 'solution' etc.*/
+function verifyExpectedProps(parser, file, props) {
+  Object.keys(props).forEach(prop => {
+    if (parser.files[file][prop] === undefined) {
+      console.warn(`Prop ${prop} missing from file ${file}`);
+    }
+  });
+}
+
+/* Formats the result as JSON. */
+function formatResult(parsedFiles) {
+  return Object.keys(parsedFiles).map(
+    filename => JSON.stringify(parsedFiles[filename])
+  );
+}
+
+/* Prints the results to given output file. */
+function printToOutput (res) {
+  if (opts.outfile) {
+    if (!fs.existsSync(opts.outfile) || opts.force) {
+      fs.writeFileSync(opts.outfile, res);
+    }
+  }
+  else {
+    console.log(res);
+  }
+}
+
+/* Prints the usage of the script and exits. */
+function usage(exitCode = 0) {
+  console.log('Usage: bin/js2json_challenges.js [opts] <files>+\n');
+  optionDefinitions.forEach(opt => {
+    if (opt.alias) {
+      console.log(`\t--${opt.name},-${opt.alias}\t- ${opt.descr}`);
+    }
+    else {
+      console.log(`\t--${opt.name}\t\t- ${opt.descr}`);
+    }
+  });
+  process.exit(exitCode);
+}
---
+++ client/scripts/challenges/queue_with_two_stacks.js
@@ -0,0 +1,82 @@
+/* eslint spaced-comment: 0 */
+const assert = require('chai').assert;
+
+/// description:
+`A queue is  a data structure with FIFO (first-in first-out) properties. Elements are removed in the same order as they are inserted. In this challenge, you need to implement Queue class using two Stack data structures.
+Instructions: Implement a Queue class with two methods: enqueue() which inserts an element into a queue and dequeue() which removes an element in an insertion order.
+`;
+
+/// head:
+function Stack() {
+  this.list = [];
+  this.size = 0;
+
+  this.size = function() {
+    return this.list.length;
+  };
+
+  this.pop = function() {
+    return this.list.pop();
+  };
+
+  this.push = function(elem) {
+    return this.list.push(elem);
+  };
+}
+
+/// challengeSeed:
+
+// class Stack:
+//    push(elem): Push new element to the top
+//    pop():  Remove and return the top element
+//    size(): Return the number of elements in Stack
+
+let Queue = function() {
+  this.s1 = new Stack();
+  this.s2 = new Stack();
+
+  this.enqueue = function(elem) {
+  };
+
+  this.dequeue = function() {
+  };
+};
+
+/// solution:
+Queue = function () {
+  this.s1 = new Stack();
+  this.s2 = new Stack();
+
+  this.enqueue = function(elem) {
+    this.s1.push(elem);
+  };
+
+  this.dequeue = function() {
+    if (this.s2.size() > 0) {
+      return this.s2.pop();
+    }
+    while (this.s1.size() > 0) {
+      this.s2.push(this.s1.pop());
+    }
+    return this.s2.pop();
+  };
+};
+
+/// tail:
+const q = new Queue();
+q.enqueue(1); q.enqueue(2); q.enqueue(3);
+
+const q2 = new Queue();
+[1, 2, 3, 4, 5, 6].forEach(num => q2.enqueue(num));
+q2.dequeue(); q2.dequeue(); q2.dequeue(); q2.dequeue();
+[7, 8, 9, 10].forEach(num => q2.enqueue(num));
+
+/// tests:
+assert(typeof Queue === 'function', 'Queue must be a constructor function.');
+
+assert(q.dequeue() === 1, 'message: 1 is dequeued correctly');
+assert(q.dequeue() === 2, 'message: 2 is dequeued correctly');
+
+assert(q2.dequeue() === 5, 'message: 5 is dequeued correctly');
+assert(q2.dequeue() === 6, 'message: 5 is dequeued correctly');
+assert(q2.dequeue() === 7, 'message: 5 is dequeued correctly');
