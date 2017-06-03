15efe950f670b9bfc8cb0e52999be629fe9f4722
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-03 22:38:44 +0200

    Added functions to extract var and member names from acorn-ast + unit tests.

---
+++ test/client/scripts/arcademode/assert-ast.spec.js
@@ -0,0 +1,26 @@
+
+import { expect } from 'chai';
+import * as Ast from '../../../../client/scripts/arcademode/assert-ast';
+
+const acorn = require('acorn');
+
+const parseOpts = {
+};
+
+describe('AssertAst', () => {
+  it('Extracts declared var names from ast', () => {
+    const ast = acorn.parse('let xxx = "abcd";', parseOpts);
+    const varNames = Ast.getVarNames(ast);
+    expect(varNames).to.have.length(1);
+    expect(varNames[0]).to.equal('xxx');
+  });
+
+  it('checks that only given variables are declared', () => {
+    const ast = acorn.parse('this.s1 = []; this.s2 = []; this.func = function() {};', parseOpts);
+    const varNames = Ast.getThisVarNames(ast);
+    expect(varNames).to.have.length(3);
+    expect(varNames).to.include('s1');
+    expect(varNames).to.include('s2');
+    expect(varNames).to.include('func');
+  });
+});
