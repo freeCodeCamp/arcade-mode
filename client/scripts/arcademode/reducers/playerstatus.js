4c429e2664f0ba2e24795de0e55d6f8a1416e3cc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-04 23:36:27 +0200

    Pass option can now be set from config file. Also default lives values are read from this file.

--- client/scripts/arcademode/reducers/playerstatus.js
+++ client/scripts/arcademode/reducers/playerstatus.js
@@ -16,25 +16,32 @@ import {
   GAME_DIFFICULTY_CHANGE
 } from '../actions/gamesetting';
 
+/*
 import {
   PLAYER_PASSED
 } from '../actions/playerstatus';
+*/
 
 import {
   MODAL_OPEN
 } from '../actions/modal';
 
+import appConfig from '../../../jsons/appconfig.json';
+
+const difficultyOptions = appConfig.options.Difficulty.options;
+
 const initialState = Immutable.Map({
-  lives: 5,
-  passOption: true
+  lives: difficultyOptions.Easy.lives,
+  passOption: appConfig.pass
 });
+
 
 // currently difficulty settings pertain to lives and timeleft only,
 // as challenges still need to be sorted by difficulty.
 const difficultySettings = {
-  Easy: { lives: 20 },
-  Medium: { lives: 10 },
-  Hard: { lives: 5 }
+  Easy: { lives: difficultyOptions.Easy.lives },
+  Medium: { lives: difficultyOptions.Medium.lives },
+  Hard: { lives: difficultyOptions.Hard.lives }
   // Random - for random, can randomly generate lives and time and hide until game start
 };
 
@@ -43,12 +50,16 @@ export default function playerstatus (state = initialState, action) {
     case CHALLENGE_START:
       return state
         .set('lives', initialState.get('lives'))
-        .set('passOption', true);
+        .set('passOption', appConfig.pass);
     case CHALLENGE_NEXT:
       return state
-        .set('passOption', true); // refresh passOption on each challenge solve
-    // case TESTS_FAILED:
-    //  return state.update('lives', lives => lives - 1);
+        .set('passOption', appConfig.pass); // refresh passOption on each challenge solve
+    case TESTS_FAILED: {
+      if (appConfig.lives) {
+        return state.update('lives', lives => lives - 1);
+      }
+      return state;
+    }
     case GAME_DIFFICULTY_CHANGE:
       return state.set('lives', difficultySettings[action.difficulty].lives);
     // case PLAYER_PASSED:
