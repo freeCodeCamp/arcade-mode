5c9b6abfd413a521fdfe0544cb465a6d9643a157
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-04 13:17:18 +0200

    Added unit tests for  testing class syntax.

--- test/client/scripts/arcademode/assert-ast.spec.js
+++ test/client/scripts/arcademode/assert-ast.spec.js
@@ -7,6 +7,30 @@ const acorn = require('acorn');
 const parseOpts = {
 };
 
+const codeFuncX = `
+function x() {
+  var localFunc = function() {};
+  this.s1 = [];
+  this.s2 = [];
+  this.func = function() {};
+}
+`;
+
+const codeClassY = `
+class Y {
+  constructor() {
+    this.v1 = [];
+    this.v2 = {};
+    this.v3 = function tt() {this.vv4 = [];};
+  }
+
+  doStuff() { }
+
+}
+`;
+
+const codeXAndY = `${codeFuncX}\n${codeClassY}`;
+
 describe('AssertAst', () => {
   it('Extracts declared var names from ast', () => {
     const ast = acorn.parse('let xxx = "abcd";', parseOpts);
@@ -16,7 +40,7 @@ describe('AssertAst', () => {
   });
 
   it('checks that only given variables are declared', () => {
-    const ast = acorn.parse('this.s1 = []; this.s2 = []; this.func = function() {};', parseOpts);
+    const ast = acorn.parse(codeFuncX, parseOpts);
     const varNames = Ast.getThisVarNames(ast);
     expect(varNames).to.have.length(3);
     expect(varNames).to.include('s1');
@@ -25,9 +49,17 @@ describe('AssertAst', () => {
   });
 
   it('extracts all called functions', () => {
-    const ast = acorn.parse('this.func = function() {}; this.func(); let a = [].sort();', parseOpts);
+    const ast = acorn.parse('this[kkk](); this.prop["xxx"].yyy(test); this.func = function() {}; this.func(); let a = [].sort();', parseOpts);
     const funcCalls = Ast.getMemberFuncCalls(ast);
-    expect(funcCalls).to.have.length(2);
+    expect(funcCalls).to.have.length(4);
     expect(funcCalls).to.include('sort');
+    expect(funcCalls).to.include('yyy');
+  });
+
+  it('extracts this members from a class', () => {
+    const ast = acorn.parse(codeClassY, parseOpts);
+    console.log(JSON.stringify(ast, null, 2));
+    const varNames = Ast.getThisVarNames(ast);
+    expect(varNames).to.have.length(6);
   });
 });
