31f4d23389abfd8070965ab7bd1912bbf6a30fb4
timolawl <timolawl@gmail.com>
2017-06-05 15:04:24 -0700

    update link

--- test/client/scripts/public/titleCanvas.spec.js
+++ test/client/scripts/public/titleCanvas.spec.js
@@ -10,7 +10,7 @@ describe('ARCADE MODE Canvas animation', () => {
 
   before(() => {
     document.body.innerHTML =
-'<a href="//arcademode.herokuapp.com" class="am__am__link"><div class="am__am__logo"><canvas class="am__am__canvas" height="50" width="220">ARCADE MODE</canvas><svg class="am__am__svg"><clipPath id="arcadePath"><text class="am__am__canvas__text" x="0" y="35">ARCADE MODE</text></clipPath></svg></div></a>';
+'<a href="//freecodecamp.github.io/arcade-mode" class="am__am__link"><div class="am__am__logo"><canvas class="am__am__canvas" height="50" width="220">ARCADE MODE</canvas><svg class="am__am__svg"><clipPath id="arcadePath"><text class="am__am__canvas__text" x="0" y="35">ARCADE MODE</text></clipPath></svg></div></a>';
 
     canvasObject.init();
   });
