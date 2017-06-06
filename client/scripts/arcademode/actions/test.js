03cb715e710de92a180b6666cc7e00555fb0eda3
timolawl <timolawl@gmail.com>
2017-06-06 11:30:21 -0700

    add statistical confidence output

--- client/scripts/arcademode/actions/test.js
+++ client/scripts/arcademode/actions/test.js
@@ -11,55 +11,6 @@ export const TESTS_PASSED = 'TESTS_PASSED'; // test
 
 const getTestStatus = testResults => testResults.every(testResult => testResult.pass);
 
-// TODO:
-// possibly reduce the number of workers because spawning and setting up a worker takes time
-
-const createTestWorker = (userCode, currChallenge, dispatch) =>
-  new Promise(resolve => {
-    const wk = new Worker('public/js/ww.bundle.js');
-    wk.postMessage([userCode, currChallenge.toJS()]); // postMessage mangles the Immutable object, so it needs to be transformed into regular JS before sending over to worker.
-    wk.onmessage = e => {
-      resolve(e.data);
-    };
-  })
-  .then(workerData => {
-    dispatch(onOutputChange(workerData[0]));
-    if (workerData.length > 1) {
-      const testResults = workerData.slice(4); // to take into account the benchmark items
-      const allTestsPassed = getTestStatus(testResults);
-
-      if (allTestsPassed) {
-        dispatch(actionTestsPassed());
-        // call benchmark webworkers here:
-
-        const benchmarkFnCall = workerData[3];
-        if (benchmarkFnCall) {
-          const benchmarkCode = workerData.slice(1, 3);
-          const benchmarkTimes = createBenchmarkWorker(benchmarkCode, benchmarkFnCall)
-            .then(data => console.log(data))
-            .catch(err => console.error(err));
-        }
-      }
-      else {
-        dispatch(actionTestsFailed());
-      }
-
-      // Would it make sense to include this with passed/failed actions?
-      dispatch(actionTestsFinished(testResults));
-    }
-  })
-  .catch(err => { console.log(`Promise rejected: ${err}.`); });
-
-const createBenchmarkWorker = (code, benchmarkFnCall) =>
-  new Promise(resolve => {
-    const wk = new Worker('public/js/wwBenchmark.bundle.js');
-    wk.postMessage([code, benchmarkFnCall]);
-    wk.onmessage = e => {
-      resolve(e.data);
-    };
-  })
-  .catch(err => console.error(err));
-
 export function onOutputChange(newOutput) {
   return {
     type: OUTPUT_CHANGED,
@@ -74,7 +25,10 @@ export function runTests(userCode, currChallenge) {
 
     // Eval user code inside worker
     // http://stackoverflow.com/questions/9020116/is-it-possible-to-restrict-the-scope-of-a-javascript-function/36255766#36255766
-    createTestWorker(userCode, currChallenge, dispatch);
+    const perfBefore = performance.now();
+    createTestWorker(userCode, currChallenge, dispatch)
+      .then(() => console.log(`Total time to evaluate test cases: ${performance.now() - perfBefore}`))
+      .catch(err => console.error(err));
   };
 }
 
@@ -106,3 +60,110 @@ export function actionTestsFinished (testResults) {
     testResults
   };
 }
+
+
+function verifyFastest (data) {
+  const re = /^.*x (\d.*) ops\/sec ±(.*)% \(.*$/;
+  let statisticalConfidence = true;
+
+  if (data.testData[0].match(re) && data.testData[1].match(re)) {
+    const stats = data.testData.map(datum => {
+      return {
+        mean: parseInt((datum.match(re)[1]).replace(',', ''), 10),
+        pMarginOfError: Number(datum.match(re)[2]) / 100
+      };
+    }).map(stat => {
+      return {
+        mean: stat.mean,
+        pMarginOfError: stat.pMarginOfError,
+        aMarginOfError: stat.mean * stat.pMarginOfError
+      };
+    });
+
+    // console.log(stats);
+
+
+    const stockRange = [
+      stats[0].mean - stats[0].aMarginOfError, stats[0].mean + stats[0].aMarginOfError
+    ];
+
+    const userRange = [
+      stats[1].mean - stats[1].aMarginOfError, stats[1].mean + stats[1].aMarginOfError
+    ];
+
+    // console.log(stockRange, userRange);
+
+    if (stockRange[0] <= userRange[1] && stockRange[1] >= userRange[0]) {
+      statisticalConfidence = false;
+    }
+
+    if (statisticalConfidence) {
+      if (data.fastest === 'user test') {
+        // user code faster than stock code:
+        console.log('Your code is extremely fast!');
+      }
+      else {
+        // stock code faster than user code:
+        console.log('Your code can be made more efficient.');
+      }
+    }
+    else {
+      // tied between user and stock code:
+      console.log('Optimal code!');
+    }
+  }
+}
+
+// TODO:
+// possibly reduce the number of workers because spawning and setting up a worker takes time
+const createTestWorker = (userCode, currChallenge, dispatch) =>
+  new Promise(resolve => {
+    const wk = new Worker('public/js/ww.bundle.js');
+    wk.postMessage([userCode, currChallenge.toJS()]); // postMessage mangles the Immutable object, so it needs to be transformed into regular JS before sending over to worker.
+    wk.onmessage = e => {
+      resolve(e.data);
+    };
+  })
+  .then(workerData => {
+    dispatch(onOutputChange(workerData[0]));
+    if (workerData.length > 1) {
+      const testResults = workerData.slice(4); // to take into account the benchmark items
+      const allTestsPassed = getTestStatus(testResults);
+
+      if (allTestsPassed) {
+        dispatch(actionTestsPassed());
+        // call benchmark webworkers here:
+        const perfBefore = performance.now();
+        const benchmarkFnCall = workerData[3];
+        if (benchmarkFnCall) {
+          const benchmarkCode = workerData.slice(1, 3);
+          const benchmarkTimes = createBenchmarkWorker(benchmarkCode, benchmarkFnCall)
+            .then(data => {
+              console.log(data.fastest);
+              verifyFastest(data);
+              console.log(data.testData[0]);
+              console.log(data.testData[1]);
+              console.log(`Total time to benchmark: ${performance.now() - perfBefore}`);
+            })
+            .catch(err => console.error(err));
+        }
+      }
+      else {
+        dispatch(actionTestsFailed());
+      }
+
+      // Would it make sense to include this with passed/failed actions?
+      dispatch(actionTestsFinished(testResults));
+    }
+  })
+  .catch(err => { console.log(`Promise rejected: ${err}.`); });
+
+const createBenchmarkWorker = (code, benchmarkFnCall) =>
+  new Promise(resolve => {
+    const wk = new Worker('public/js/wwBenchmark.bundle.js');
+    wk.postMessage([code, benchmarkFnCall]);
+    wk.onmessage = e => {
+      resolve(e.data);
+    };
+  })
+  .catch(err => console.error(err));
