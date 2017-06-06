530c0052a898e8518b8175eba6ba70fe25e793c7
timolawl <timolawl@gmail.com>
2017-06-05 17:54:03 -0700

    full refactor

--- client/scripts/arcademode/persistdata.js
+++ client/scripts/arcademode/persistdata.js
@@ -8,138 +8,64 @@ import { fromJS } from 'immutable';
 const debug = require('debug')('am:persistdata');
 
 const USER_DATA_KEY = 'userData';
+const IDB_VERSION = 1;
 
-/* Class for persisting and loading persisted user data. */
-export default class Persist {
-
-  /* Takes the storage used as argument. Throws with storage. */
-  constructor(storage) {
-    if (storage) {
-      this.storage = storage;
-    }
-    else {
-      throw new Error('A storage object must be passed to new Persist()');
-    }
-  }
-
-  // needed to ensure that indexedDB is initialized before other methods can access it.
-  init () {
-    // initialize indexedDB:
-    const DBOpenRequest = this.storage.open(USER_DATA_KEY, 1);
-
-    DBOpenRequest.onupgradeneeded = () => {
-      const db = DBOpenRequest.result;
-      db.createObjectStore('userData', { keyPath: 'id', autoIncrement: true });
+function connectToIDB (name, version) {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open(name, version);
+    request.onupgradeneeded = () => {
+      request.result.createObjectStore('userData', { keyPath: 'id', autoIncrement: true });
     };
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+    request.onblocked = () => { debug('blocked'); };
+  });
+}
 
-    return new Promise((resolve, reject) => {
-      DBOpenRequest.onerror = event => {
-        reject(event);
-      };
-
-      DBOpenRequest.onsuccess = event => {
-        debug(`IndexedDB successfully opened: ${event.target.result}`);
-        resolve(event.result);
-        this.db = DBOpenRequest.result;
-      };
-    });
-  }
-
-  /* Gets all user data from storage. */
-  fromStorage() {
-    return this.init().then(() => {
-      const db = this.db;
-      const tx = db.transaction('userData');
-      const store = tx.objectStore('userData');
-      const userData = store.getAll();
-
-      return new Promise((resolve, reject) => {
-        userData.onsuccess = () => {
-          debug(`Data in storage found!: ${userData.result}`);
-          // return proper form:
-          // const obj = {};
-          // obj.sessions = fromJS(userData.result);
-          resolve({ sessions: fromJS(userData.result) });
-        };
-
-        userData.onerror = () => {
-          debug('Data storage retrieval error.');
-          reject(userData.error);
-        };
-      });
-    });
-  }
-
-  /* Saves the given session data into storage. */
-  toStorage(session) {
-    if (!this.db) {
-      return this.init().then(() => {
-        const db = this.db;
-        const tx = db.transaction('userData', 'readwrite');
-        const store = tx.objectStore('userData');
-        const putUserData = store.put(session.toJS());
-
-        return new Promise((resolve, reject) => {
-          putUserData.onsuccess = () => {
-            resolve(putUserData.result);
-          };
-
-          putUserData.onerror = () => {
-            reject(putUserData.error);
-          };
-        });
-      });
+function operateOnIDB (IDB, operation, data) {
+  return new Promise((resolve, reject) => {
+    const transaction = IDB.transaction('userData', 'readwrite');
+    const store = transaction.objectStore('userData');
+    let request;
+    switch (operation) {
+      case 'GET':
+        request = store.getAll();
+        request.onsuccess = () => resolve({ sessions: fromJS(request.result) });
+        request.onerror = () => reject(request.error);
+        break;
+      case 'PUT':
+        request = store.put(data.toJS());
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+        break;
+      case 'DEL':
+        request = store.delete(data.toJS().id);
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+        break;
+      default: debug('No operation performed on IDB');
     }
+  });
+}
 
-    const db = this.db;
-    const tx = db.transaction('userData', 'readwrite');
-    const store = tx.objectStore('userData');
-    const putUserData = store.put(session.toJS());
-
-    return new Promise((resolve, reject) => {
-      putUserData.onsuccess = () => {
-        resolve(putUserData.result);
-      };
-
-      putUserData.onerror = () => {
-        reject(putUserData.error);
-      };
-    });
+async function operateWithIDB (operation, data) {
+  let IDB;
+  try {
+    IDB = await connectToIDB(USER_DATA_KEY, IDB_VERSION);
+    return await operateOnIDB(IDB, operation, data);
   }
-
-  deleteFromStorage(session) {
-    if (!this.db) {
-      return this.init().then(() => {
-        const db = this.db;
-        const tx = db.transaction('userData', 'readwrite');
-        const store = tx.objectStore('userData');
-        const removeUserData = store.delete(session.toJS().id);
-
-        return new Promise((resolve, reject) => {
-          removeUserData.onsuccess = () => {
-            resolve(removeUserData.result);
-          };
-
-          removeUserData.onerror = () => {
-            reject(removeUserData.error);
-          };
-        });
-      });
+  catch (exception) {
+    debug(exception);
+  }
+  finally {
+    if (IDB) {
+      IDB.close();
     }
-
-    const db = this.db;
-    const tx = db.transaction('userData', 'readwrite');
-    const store = tx.objectStore('userData');
-    const removeUserData = store.delete(session.toJS().id);
-
-    return new Promise((resolve, reject) => {
-      removeUserData.onsuccess = () => {
-        resolve(removeUserData.result);
-      };
-
-      removeUserData.onerror = () => {
-        reject(removeUserData.error);
-      };
-    });
   }
+}
+
+export default function Persist () {
+  this.fromStorage = () => operateWithIDB('GET', null);
+  this.toStorage = data => operateWithIDB('PUT', data);
+  this.deleteFromStorage = data => operateWithIDB('DEL', data);
 }
