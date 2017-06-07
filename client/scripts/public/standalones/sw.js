f0488febd2b0c5138da435a0a4921ded0167921d
timolawl <timolawl@gmail.com>
2017-06-07 07:29:32 -0700

    cache bust update

--- client/scripts/public/standalones/sw.js
+++ client/scripts/public/standalones/sw.js
@@ -10,7 +10,7 @@ import * as CACHE from './swCache.json';
 
 const debug = require('debug')('am:sw');
 
-const CACHE_VERSION = 5;
+const CACHE_VERSION = 6;
 const CURRENT_CACHES = {
   font: `font-cache-v${CACHE_VERSION}`,
   css: `css-cache-v${CACHE_VERSION}`,
