6bea70a6956af7a2932092c2dbaf34b80991b62f
timolawl <timolawl@gmail.com>
2017-06-08 18:56:19 -0700

    update tests

--- test/client/scripts/arcademode/runner.spec.js
+++ test/client/scripts/arcademode/runner.spec.js
@@ -38,78 +38,98 @@ const challengeWithTail = {
 describe('runner()', () => {
   it('executes js using eval and returns results', () => {
     const userCode = '// no code';
-    const res = runner(userCode, emptyChallenge);
-    expect(res).to.have.property('userOutput');
-    expect(res).to.have.property('errorMsgs');
-    expect(res).to.have.property('testResults');
-    expect(res.errorMsgs).to.have.length(0);
+    const result = runner(userCode, emptyChallenge);
+    return result.then(res => {
+      expect(res).to.have.property('userOutput');
+      expect(res).to.have.property('errorMsgs');
+      expect(res).to.have.property('testResults');
+      expect(res.errorMsgs).to.have.length(0);
+    });
   });
 
   it('catches invalid syntax correctly', () => {
     const userCode = 'syntaxError [][ = ;];';
-    const res = runner(userCode, emptyChallenge);
-    expect(res.errorMsgs).to.have.length(1);
-    expect(res.userOutput).to.have.length.above(0);
-    expect(res.userOutput).to.match(/SyntaxError/);
-    expect(res.testResults).to.have.length(0);
+    const result = runner(userCode, emptyChallenge);
+    return result.then(res => {
+      expect(res.errorMsgs).to.have.length(1);
+      expect(res.userOutput).to.have.length.above(0);
+      expect(res.userOutput).to.match(/SyntaxError/);
+      expect(res.testResults).to.have.length(0);
+    });
   });
 
   it('catches invalid execution, non-exec code', () => {
     const userCode = 'let myVar = NonExistingClass()';
-    const res = runner(userCode, emptyChallenge);
-    expect(res.errorMsgs).to.have.length(1);
-    expect(res.userOutput).to.have.length.above(0);
-    expect(res.userOutput).to.match(/Error:/);
-    expect(res.testResults).to.have.length(0);
+    const result = runner(userCode, emptyChallenge);
+    return result.then(res => {
+      expect(res.errorMsgs).to.have.length(1);
+      expect(res.userOutput).to.have.length.above(0);
+      expect(res.userOutput).to.match(/Error:/);
+      expect(res.testResults).to.have.length(0);
+    });
   });
 
   it('Runs tests for correct code', () => {
     const userCode = '// Error-free code\ntrue;';
-    const res = runner(userCode, challengeWithTest);
-    expect(res.testResults).to.have.length(1);
-    expect(res.errorMsgs).to.have.length(0);
-    expect(res.userOutput).to.equal('true');
+    const result = runner(userCode, challengeWithTest);
+    return result.then(res => {
+      expect(res.testResults).to.have.length(1);
+      expect(res.errorMsgs).to.have.length(0);
+      expect(res.userOutput).to.equal('true');
+    });
   });
 
   it('adds head before executing the code', () => {
     const userCode = 'seenByUser();';
-    const res = runner(userCode, challengeWithHead);
-    expect(res.userOutput).to.equal('2');
+    const result = runner(userCode, challengeWithHead);
+    return result.then(res => {
+      expect(res.userOutput).to.equal('2');
+    });
   });
 
   it('adds tail before running the tests', () => {
     const userCode = '// No user code really';
-    const res = runner(userCode, challengeWithTail);
-    expect(res.userOutput).to.equal(userOutputUndef);
+    const result = runner(userCode, challengeWithTail);
+    return result.then(res => {
+      expect(res.userOutput).to.equal(userOutputUndef);
+    });
   });
 
   it('catches infinite loops in user code', () => {
     const userCode = 'while (true);';
-    const res = runner(userCode, challengeWithHead);
-    expect(res.errorMsgs).to.have.length(1);
-    expect(res.userOutput).to.match(/infinite loop/);
+    const result = runner(userCode, challengeWithHead);
+    return result.then(res => {
+      expect(res.errorMsgs).to.have.length(1);
+      expect(res.userOutput).to.match(/infinite loop/);
+    });
   });
 
   it('can execute queue-2-stack challenge', () => {
     let userCode = stackChallenge.solutions.join('');
     userCode = userCode.replace(/^\s*Queue/, 'const Queue');
-    const res = runner(userCode, stackChallenge);
-    expect(res.userOutput).to.equal(userOutputUndef);
-    expectNoErrorsAndAllTestsRun(res, stackChallenge);
+    const result = runner(userCode, stackChallenge);
+    return result.then(res => {
+      expect(res.userOutput).to.equal(userOutputUndef);
+      expectNoErrorsAndAllTestsRun(res, stackChallenge);
+    });
   });
 
   it('can execute the book shop challenge', () => {
     const userCode = bookshopChallenge.solutions.join('');
-    const res = runner(userCode, bookshopChallenge);
-    expect(res.userOutput).to.equal(userOutputUndef);
-    expectNoErrorsAndAllTestsRun(res, bookshopChallenge);
+    const result = runner(userCode, bookshopChallenge);
+    return result.then(res => {
+      expect(res.userOutput).to.equal(userOutputUndef);
+      expectNoErrorsAndAllTestsRun(res, bookshopChallenge);
+    });
   });
 
   it('can execute the gittaca challenge', () => {
     const userCode = gittacaChallenge.solutions.join('');
-    const res = runner(userCode, gittacaChallenge);
-    expect(res.userOutput).to.equal(userOutputUndef);
-    expectNoErrorsAndAllTestsRun(res, gittacaChallenge);
+    const result = runner(userCode, gittacaChallenge);
+    return result.then(res => {
+      expect(res.userOutput).to.equal(userOutputUndef);
+      expectNoErrorsAndAllTestsRun(res, gittacaChallenge);
+    });
   });
 });
 
