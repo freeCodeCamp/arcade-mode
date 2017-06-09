f355ea57f37b4bd6aec58cb55f4757b5daf29e08
timolawl <timolawl@gmail.com>
2017-06-08 19:00:29 -0700

    add async/await polyfill; revise to promise-based

--- client/scripts/public/standalones/ww.js
+++ client/scripts/public/standalones/ww.js
@@ -6,12 +6,30 @@
 
 'use strict';
 
+import 'babel-polyfill';
+
 import runner from '../../arcademode/runner';
 
 self.onmessage = e => {
   const userCode = e.data[0];
   const currChallenge = e.data[1];
-  const data = runner(userCode, currChallenge);
+  const rawData = runner(userCode, currChallenge);
+
+  let postData;
+
+  rawData.then(data => {
+    postData = [
+      data.userOutput,
+      data.benchmarkStockCode,
+      data.benchmarkUserCode,
+      data.benchmarkFnCall,
+      ...data.errorMsgs,
+      ...data.testResults
+    ];
+    // post message back to main thread:
+    self.postMessage(postData);
+  });
+/*
   const postData = [
     data.userOutput,
     data.benchmarkStockCode,
@@ -20,8 +38,6 @@ self.onmessage = e => {
     ...data.errorMsgs,
     ...data.testResults
   ];
-
-  // post message back to main thread:
-  self.postMessage(postData);
+*/
 };
 
