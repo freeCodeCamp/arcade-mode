6128c9ef8a5439f4b0cf5b6e8a5fff1751c51265
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-17 00:01:55 +0200

    Added unit tests for rosetta code challenges.

---
+++ test/client/scripts/challenges/rosettacode.js
@@ -0,0 +1,36 @@
+
+import { expect } from 'chai';
+
+import runner from '../../../../client/scripts/arcademode/runner';
+
+import RosettaChallenges from '../../../../public/json/challenges-rosetta.json';
+
+const challenges = RosettaChallenges.challenges;
+
+const userOutputUndef = 'User output is undefined.';
+
+challenges.forEach(challenge => {
+  describe(`RosettaChallenge ${challenge.title}`, () => {
+    it('runner can execute all the challenge properly', () => {
+      console.log(`Executing challenge ${challenge.title}`);
+
+      const userCode = challenge.solutions.join('');
+      const result = runner(userCode, challenge);
+      expect(challenge.tests).to.have.property('length');
+
+      return result.then(res => {
+        expect(res.userOutput).to.equal(userOutputUndef);
+        expectNoErrorsAndAllTestsRun(res, challenge);
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+    });
+  });
+});
+
+// HELPER FUNCTIONS
+function expectNoErrorsAndAllTestsRun(res, challenge) {
+  expect(res.errorMsgs).to.have.length(0);
+  expect(res.testResults).to.have.length(challenge.tests.length);
+}
