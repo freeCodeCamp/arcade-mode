a5ba2f198977487230659f808d7bfe4cd6cda1aa
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-19 00:40:38 +0200

    Added a test to check that test fails without proper solution. Added checks that each result.pass is true for correct solution.

--- test/client/scripts/challenges/rosettacode.js
+++ test/client/scripts/challenges/rosettacode.js
@@ -9,19 +9,35 @@ const challenges = RosettaChallenges.challenges;
 
 const userOutputUndef = 'User output is undefined.';
 
+describe('Rosetta challenges', () => {
+  it('fails to pass at least one test without solution', () => {
+    const challenge = challenges.find(item => item.title.match(/Zig/));
+    const userCode = challenge.challengeSeed.join('\n');
+    const result = runner(userCode, challenge);
+
+    return result.then(res => {
+      expect(res.errorMsgs).to.have.length(0);
+      expect(res.testResults).to.have.length(challenge.tests.length);
+
+      let atLeastOneTestFailed = false;
+      res.testResults.forEach(tr => {
+        atLeastOneTestFailed = atLeastOneTestFailed || tr.pass === false;
+      });
+      expect(atLeastOneTestFailed).to.equal(true);
+    });
+  });
+});
+
 challenges.forEach(challenge => {
   describe(`RosettaChallenge ${challenge.title}`, () => {
     it(`runner can execute ${challenge.title} challenge properly`, () => {
       const userCode = challenge.solutions.join('');
-      const result = runner(userCode, challenge);
       expect(challenge.tests).to.have.property('length');
+      const result = runner(userCode, challenge);
 
       return result.then(res => {
         expect(res.userOutput).to.equal(userOutputUndef);
         expectNoErrorsAndAllTestsRun(res, challenge);
-      })
-      .catch(err => {
-        throw new Error(err);
       });
     });
   });
@@ -31,4 +47,7 @@ challenges.forEach(challenge => {
 function expectNoErrorsAndAllTestsRun(res, challenge) {
   expect(res.errorMsgs).to.have.length(0);
   expect(res.testResults).to.have.length(challenge.tests.length);
+  res.testResults.forEach(result => {
+    expect(result.pass).to.be.true;
+  });
 }
