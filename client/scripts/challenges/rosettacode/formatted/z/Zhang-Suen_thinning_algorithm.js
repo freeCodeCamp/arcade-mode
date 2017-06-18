8fae95a21400abe63ffa599f99c205278f67f2bb
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-06-18 18:58:26 +0200

    Cleaned up the description formatting.

--- client/scripts/challenges/rosettacode/formatted/z/Zhang-Suen_thinning_algorithm.js
+++ client/scripts/challenges/rosettacode/formatted/z/Zhang-Suen_thinning_algorithm.js
@@ -1,5 +1,8 @@
 /* eslint spaced-comment: 0 */
 /* eslint no-continue: 0 */
+/* eslint no-unused-vars: 0 */
+/* eslint no-shadow: 0 */
+/* eslint no-redeclare: 0 */
 
 const assert = require('chai').assert;
 
@@ -9,7 +12,7 @@ const assert = require('chai').assert;
 
 /// description:
 /// This is an algorithm used to thin a black and white i.e. one bit per pixel images.
-///
+///<br/>
 /// For example, with an input image of:
 /// <pre>
 ///  #################                   #############
@@ -48,22 +51,22 @@ const assert = require('chai').assert;
 ///                        ###                          ###
 ///
 ///                                                            </pre>
-///
-/// ;Algorithm:
+///<br/>
+/// <h2>Algorithm</h2>
 /// Assume black pixels are one and white pixels zero, and that the input image is a rectangular N by M array of ones and zeroes.
-///
+///<br/>
 /// The algorithm operates on all black pixels P1 that can have eight neighbours. The neighbours are, in order, arranged as:
 /// <table border="1">
 ///   <tr><td>P9</td><td>P2</td><td>P3</td></tr>
 ///   <tr><td>P8</td><td><b>P1</b></td><td>P4</td></tr>
 ///   <tr><td>P7</td><td>P6</td><td>P5</td></tr>
 /// </table>
-///
+///<br/>
 /// Obviously the boundary pixels of the image cannot have the full eight neighbours.
-///
+///<br/>
 /// * Define <math>A(P1)</math> = the number of transitions from white to black, (0 -> 1) in the sequence P2,P3,P4,P5,P6,P7,P8,P9,P2. (Note the extra P2 at the end - it is circular).
 /// * Define <math>B(P1)</math> = The number of black pixel neighbours of P1. ( = sum(P2 .. P9) )
-///
+///<br/>
 /// ;Step 1:
 /// All pixels are tested and pixels satisfying all the following conditions (simultaneously) are just noted at this stage.
 /// * (0) The pixel is black and has eight neighbours
@@ -72,7 +75,7 @@ const assert = require('chai').assert;
 /// * (3) At least one of P2 and P4 and P6 is white
 /// * (4) At least one of P4 and P6 and P8 is white
 /// After iterating over the image and collecting all the pixels satisfying all step 1 conditions, all these condition satisfying pixels are set to white.
-///
+///<br/>
 /// ;Step 2:
 /// All pixels are again tested and pixels satisfying all the following conditions are just noted at this stage.
 /// * (0) The pixel is black and has eight neighbours
@@ -81,10 +84,10 @@ const assert = require('chai').assert;
 /// * (3) At least one of P2 and P4 and '''P8''' is white
 /// * (4) At least one of '''P2''' and P6 and P8 is white
 /// After iterating over the image and collecting all the pixels satisfying all step 2 conditions, all these condition satisfying pixels are again set to white.
-///
+///<br/>
 /// ;Iteration:
 /// If any pixels were set in this round of either step 1 or step 2 then all steps are repeated until no image pixels are so changed.
-///
+///<br/>
 /// ;Task:
 /// # Write a routine to perform Zhang-Suen thinning on an image matrix of ones and zeroes.
 
@@ -209,7 +212,7 @@ const ZhangSuen = (function () {
   };
 
   ZhangSuen.getResult = function () {
-    let result = [];
+    const result = [];
     for (let i = 0; i < ZhangSuen.grid.length; i++) {
       const row = ZhangSuen.grid[i].join('');
       result.push(row);
@@ -224,6 +227,26 @@ function thinImage(image) {
 }
 
 /// tail:
+
+const imageForTests = [
+  '                                                          ',
+  ' #################                   #############        ',
+  ' ##################               ################        ',
+  ' ###################            ##################        ',
+  ' ########     #######          ###################        ',
+  '   ######     #######         #######       ######        ',
+  '   ######     #######        #######                      ',
+  '   #################         #######                      ',
+  '   ################          #######                      ',
+  '   #################         #######                      ',
+  '   ######     #######        #######                      ',
+  '   ######     #######        #######                      ',
+  '   ######     #######         #######       ######        ',
+  ' ########     #######          ###################        ',
+  ' ########     ####### ######    ################## ###### ',
+  ' ########     ####### ######      ################ ###### ',
+  ' ########     ####### ######         ############# ###### ',
+  '                                                          '];
 const expected = [
   '                                                          ',
   '                                                          ',
@@ -244,7 +267,10 @@ const expected = [
   '                                                          ',
   '                                                          '
 ];
+const result = thinImage(imageForTests);
 
 /// tests:
 assert.equal(typeof thinImage, 'function', 'thinImage must be a function');
-assert.deepEqual(thinImage(testImage), expected);
+assert.equal(typeof result, 'object', 'thinImage must return an array of strings');
+assert.equal(typeof result[0], 'string', 'thinImage must return an array of strings');
+assert.deepEqual(result, expected, 'thinImage must return an array of strings');
