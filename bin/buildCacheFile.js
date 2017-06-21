98137548225503c40a8274054a11ea0432bf18cf
timolawl <timolawl@gmail.com>
2017-06-20 23:08:49 -0700

    update script to regenerate file only on changes

--- bin/buildCacheFile.js
+++ bin/buildCacheFile.js
@@ -9,13 +9,19 @@
 const fs = require('fs');
 const readdir = require('recursive-readdir');
 
+const filePath = 'client/scripts/public/standalones/swCache.json';
+
 readdir('public').then(files => {
   const CACHE = {};
   CACHE.html = ['/'];
 
+  // sorted so that arbitrary file path string rearrangements in their respective arrays
+  // do not happen and thus prevents unnecessary commits
+  const sortedFiles = files.sort();
+
   const re = /^public\/([^/]*)\/.*$/;
 
-  files.forEach(file => {
+  sortedFiles.forEach(file => {
     const directory = file.match(re) && file.match(re)[1];
     if (directory) {
       if (!CACHE[directory]) {
@@ -25,7 +31,25 @@ readdir('public').then(files => {
     }
   });
 
-  const cache = JSON.stringify(CACHE, null, 2);
-  fs.writeFileSync('client/scripts/public/standalones/swCache.json', cache);
+  // If the file exists,
+  if (fs.existsSync(filePath)) {
+    const oldCache = JSON.parse(fs.readFileSync(filePath));
+
+    // check if it has been updated. If so, write over old version.
+    const updated = !Array.prototype.concat(...Object.values(CACHE))
+      .every(file => Array.prototype.concat(...Object.values(oldCache)).includes(file));
+
+    if (updated) {
+      console.log('buildCacheFile.js: Service Worker file paths used for caching have been updated!');
+      const cache = JSON.stringify(CACHE, null, 2);
+      fs.writeFileSync(filePath, cache);
+    }
+    else console.log('buildCacheFile.js: SW cache files have not changed. No operation.');
+  } // Else write the file as it is completely new.
+  else {
+    console.log('buildCacheFile.js: Service Worker file paths used for caching have been generated!');
+    const cache = JSON.stringify(CACHE, null, 2);
+    fs.writeFileSync(filePath, cache);
+  }
 })
 .catch(err => console.error(`error: ${err}`));
