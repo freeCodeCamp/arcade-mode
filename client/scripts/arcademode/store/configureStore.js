6391d63760947e7c92430f50ba04340ec992cffd
timolawl <timolawl@gmail.com>
2017-06-22 17:14:54 -0700

    comment out unused

--- client/scripts/arcademode/store/configureStore.js
+++ client/scripts/arcademode/store/configureStore.js
@@ -3,7 +3,7 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import invariant from 'redux-immutable-state-invariant';
+// import invariant from 'redux-immutable-state-invariant';
 
 import appReducer from '../reducers';
 import composeEnhancers from '../reducers/composeEnhancers';
