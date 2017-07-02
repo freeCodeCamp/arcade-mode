7f5efb8e8bbd62483ec76100b9b28e2852bcf242
timolawl <timolawl@gmail.com>
2017-07-01 18:57:07 -0700

    remove whitelist of no-undef for eslint

--- bin/fetchAndProcessRosettaCodeTasks.js
+++ bin/fetchAndProcessRosettaCodeTasks.js
@@ -235,7 +235,6 @@ function toInHouseTemplate (title, categories, description, solution, rawSolutio
   const templatedContent = `
 /* eslint spaced-comment: 0 */
 /* eslint no-redeclare: 0 */
-/* eslint no-undef: 0 */
 /* eslint no-unused-vars: 0 */
 
 const assert = require('chai').assert;
