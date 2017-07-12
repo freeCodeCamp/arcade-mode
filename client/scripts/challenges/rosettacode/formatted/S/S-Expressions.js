a655768bc7b26f735da201796c336ec98192ef41
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-12 22:30:08 +0200

    Cleaned up the HTML description.

--- client/scripts/challenges/rosettacode/formatted/S/S-Expressions.js
+++ client/scripts/challenges/rosettacode/formatted/S/S-Expressions.js
@@ -19,15 +19,45 @@ parseSexpr('((data "quoted data" 123 4.5) (data (!@# (4.5) "(more" "data)")))');
 
 /// description:
 /// <div class="rosetta">
-/// <p class="rosetta__paragraph"><a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/S-Expression" title="wp: S-Expression">S-Expressions</a>  are one convenient way to parse and store data.</p>
+/// <p class="rosetta__paragraph">
+/// <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/S-Expression" title="wp: S-Expression">S-Expressions</a> are one convenient way to parse and store data.
+/// </p>
 /// <br/><dl class="rosetta__description-list"><dt class="rosetta__description-title">Task:</dt></dl>
-/// <p class="rosetta__paragraph">Write a simple reader and writer for S-Expressions that handles quoted and unquoted strings, integers and floats.</p><br/><p class="rosetta__paragraph">The reader should read a single but nested S-Expression from a string and store it in a suitable datastructure (list, array, etc).</p><br/><p class="rosetta__paragraph">Newlines and other whitespace may be ignored unless contained within a quoted string.</p><br/><p class="rosetta__paragraph">“<tt>()</tt>”  inside quoted strings are not interpreted, but treated as part of the string.</p><br/><p class="rosetta__paragraph">Handling escaped quotes inside a string is optional;  thus “<tt>(foo"bar)</tt>” maybe treated as a string “<tt>foo"bar</tt>”, or as an error.</p><br/><p class="rosetta__paragraph">For this, the reader need not recognize “<tt>\</tt>” for escaping, but should, in addition, recognize numbers if the language has appropriate datatypes.</p><br/><p class="rosetta__paragraph">Languages that support it may treat unquoted strings as symbols.</p><br/><p class="rosetta__paragraph">Note that with the exception of “<tt>()"</tt>” (“<tt>\</tt>” if escaping is supported) and whitespace there are no special characters. Anything else is allowed without quotes.</p><br/><p class="rosetta__paragraph">The reader should be able to read the following input</p>
-/// <p class="rosetta__paragraph"><lang lisp>((data "quoted data" 123 4.5)</p>
-/// <p class="rosetta__paragraph">(data (!@# (4.5) "(more" "data)")))</lang></p>
-/// <p class="rosetta__paragraph">and turn it into a native datastructure. (see the <a class="rosetta__link--rosetta" href="http://rosettacode.org/wiki/#Pike" title="#Pike">Pike</a>, <a class="rosetta__link--rosetta" href="http://rosettacode.org/wiki/#Python" title="#Python">Python</a> and <a class="rosetta__link--rosetta" href="http://rosettacode.org/wiki/#Ruby" title="#Ruby">Ruby</a> implementations for examples of native data structures.)</p><br/><p class="rosetta__paragraph">The writer should be able to take the produced list and turn it into a new S-Expression.</p>
-/// <p class="rosetta__paragraph">Strings that don't contain whitespace or parentheses () don't need to be quoted in the resulting S-Expression, but as a simplification, any string may be quoted.</p>
-/// <br/><dl class="rosetta__description-list"><dt class="rosetta__description-title">Extra Credit:</dt></dl>
-/// <p class="rosetta__paragraph">Let the writer produce pretty printed output with indenting and line-breaks.</p>
+/// <p class="rosetta__paragraph">
+///     Write a simple reader/parser for S-Expressions that handles quoted and unquoted strings, integers and floats.
+/// </p>
+/// <p class="rosetta__paragraph">
+/// The function should read a single but nested S-Expression from a string and
+/// return it as a (nested) array.
+/// </p>
+/// <p class="rosetta__paragraph">
+///     Newlines and other whitespace may be ignored unless contained within a quoted string.
+/// </p>
+/// <p class="rosetta__paragraph">“<tt>()</tt>”  inside quoted strings are not interpreted, but treated as part of the string.
+/// </p>
+/// <p class="rosetta__paragraph">
+/// Handling escaped quotes inside a string is optional;  thus “<tt>(foo"bar)</tt>” maybe treated as a string “<tt>foo"bar</tt>”, or as an error.
+/// </p>
+/// <p class="rosetta__paragraph">
+/// For this, the reader need not recognize “<tt>\</tt>” for escaping, but should, in addition, recognize numbers if the language has appropriate datatypes.
+/// </p>
+/// <p class="rosetta__paragraph">
+/// Note that with the exception of “<tt>()"</tt>” (“<tt>\</tt>” if escaping is supported) and whitespace there are no special characters. Anything else is allowed without quotes.
+/// </p>
+/// <p class="rosetta__paragraph">The reader should be able to read the following input</p>
+/// <p class="rosetta__paragraph">
+/// <pre>
+///     ((data "quoted data" 123 4.5)
+///     (data (!@# (4.5) "(more" "data)")))
+/// </pre>
+/// </p>
+/// <p class="rosetta__paragraph">
+/// and turn it into a native datastructure. (see the
+/// <a class="rosetta__link--rosetta" href="http://rosettacode.org/wiki/#Pike" title="#Pike">Pike</a>, 
+/// <a class="rosetta__link--rosetta" href="http://rosettacode.org/wiki/#Python" title="#Python">Python</a> and
+/// <a class="rosetta__link--rosetta" href="http://rosettacode.org/wiki/#Ruby" title="#Ruby">Ruby</a> implementations
+/// for examples of native data structures.)
+/// </p>
 /// </div>
 
 /// challengeSeed:
