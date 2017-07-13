d3e1f5ce5d788d727f7be71f557b5127321ebc56
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-07-13 22:27:37 +0200

    Re-formatting of challenge HTML descriptions.

--- client/scripts/challenges/rosettacode/formatted/Y/Y_combinator.js
+++ client/scripts/challenges/rosettacode/formatted/Y/Y_combinator.js
@@ -15,12 +15,33 @@ const assert = require('chai').assert;
 /// recursion
 
 /// description:
-/// In strict <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/Functional programming" title="wp: functional programming">functional programming</a> and the <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/lambda calculus" title="wp: lambda calculus">lambda calculus</a>, functions (lambda expressions) don't have state and are only allowed to refer to arguments of enclosing functions. This rules out the usual definition of a recursive function wherein a function is associated with the state of a variable and this variable's state is used in the body of the function.<br/>
-/// The <a href="http://mvanier.livejournal.com/2897.html">Y combinator</a> is itself a stateless function that, when applied to another stateless function, returns a recursive version of the function. The Y combinator is the simplest of the class of such functions, called <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/Fixed-point combinator" title="wp: fixed-point combinator">fixed-point combinators</a>.<br/>
-/// Define the stateless Y combinator function and use it to compute
-/// <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/Factorial" title="wp: factorial">factorial</a>.
+/// <div class="rosetta">
+/// <p class="rosetta__paragraph">
+/// In strict 
+/// <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/Functional programming" title="wp: functional programming">functional programming</a> and
+/// the <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/lambda calculus" title="wp: lambda calculus">lambda calculus</a>, 
+/// functions (lambda expressions) don't have state and are only allowed to refer to arguments of enclosing functions. 
+/// This rules out the usual definition of a recursive function wherein a function is associated with the state of a variable and this variable's state is used in the body of the function.
+/// </p>
+/// <br>
+/// <p class="rosetta__paragraph">
+/// The <a href="http://mvanier.livejournal.com/2897.html">Y combinator</a> is itself a stateless function that,
+/// when applied to another stateless function, returns a recursive version of the function. The Y combinator is
+/// the simplest of the class of such functions, called 
+/// <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/Fixed-point combinator" title="wp: fixed-point combinator">fixed-point combinators</a>.
+/// </p>
+/// <br>
+/// <dl class="rosetta__description-list"><dt class="rosetta__description-title">Task:</dt></dl>
+/// <ul class="rosetta__unordered-list">
+///   <li class="rosetta__list-item--unordered">
+///     Define the stateless Y combinator function and use it to compute
+///     <a class="rosetta__link--wiki" href="https://en.wikipedia.org/wiki/Factorial" title="wp: factorial">factorial</a>.
+///   </li>
+/// </ul>
+/// <br>
 /// <code>factorial(N)</code> function is already given to you.
 /// See also <a href="http://vimeo.com/45140590">Jim Weirich: Adventures in Functional Programming</a>.
+/// </div>
 
 /// challengeSeed:
 function Y(f) {
