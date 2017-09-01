b11422052f2ad0adb083ddd89ead63976be2f930
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-09-01 09:27:59 +0300

    Ran through eslint --fix.

--- client/scripts/challenges/rosettacode/formatted/E/Element-wise operations.js
+++ client/scripts/challenges/rosettacode/formatted/E/Element-wise operations.js
@@ -7,7 +7,7 @@ const assert = require('chai').assert;
 
 /// title: Element-wise operations
 /// type: rosetta-code
-	1`
+
 /// categories:
 /// Mathematics
 
@@ -25,36 +25,36 @@ const assert = require('chai').assert;
 /// </div>
 
 /// challengeSeed:
-function operation (op,arr1,arr2) {
+function operation (op, arr1, arr2) {
   // Good luck!
 }
 
 /// solutions:
-function operation(op,arr1,arr2){
-    let ops={
-        "add":((a,b)=>a+b), 
-        "sub":((a,b)=>a-b), 
-        "mult":((a,b)=>a*b), 
-        "div":((a,b)=>a/b),
-        "exp":((a,b)=>Math.pow(a,b))
+function operation(op, arr1, arr2) {
+  const ops = {
+    add: ((a, b) => a + b),
+    sub: ((a, b) => a - b),
+    mult: ((a, b) => a * b),
+    div: ((a, b) => a / b),
+    exp: ((a, b) => a ** b)
+  };
+  const ifm = op.startsWith('m');
+  const doOp = ops[op.substring(2)];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[0].length; j++) {
+      arr1[i][j] = doOp(arr1[i][j], (ifm) ? (arr2[i][j]) : (arr2));
     }
-    let ifm=op.startsWith("m");
-    op=ops[op.substring(2)];
-    for(let i=0;i<arr1.length;i++){
-        for(let j=0;j<arr1[0].length;j++){
-            arr1[i][j]=op(arr1[i][j],(ifm)?(arr2[i][j]):(arr2));
-        }
-    }
-    return arr1;
+  }
+  return arr1;
 }
 
 /// tests:
 assert(typeof operation === 'function', 'message: <code>operation</code> is a function.');
-assert.deepEqual(operation("m_add",[[1,2],[3,4]],[[1,2],[3,4]]),[[2,4],[6,8]],'message: <code>operation("m_add",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[2,4],[6,8]]</code>.');
-assert.deepEqual(operation("s_add",[[1,2],[3,4]],2),[[3,4],[5,6]],'message: <code>operation("s_add",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[3,4],[5,6]]</code>.');
-assert.deepEqual(operation("m_sub",[[1,2],[3,4]],[[1,2],[3,4]]),[[0,0],[0,0]],'message: <code>operation("m_sub",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[0,0],[0,0]]</code>.');
-assert.deepEqual(operation("m_mult",[[1,2],[3,4]],[[1,2],[3,4]]),[[1,4],[9,16]],'message: <code>operation("m_mult",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[1,4],[9,16]]</code>.');
-assert.deepEqual(operation("m_div",[[1,2],[3,4]],[[1,2],[3,4]]),[[1,1],[1,1]],'message: <code>operation("m_div",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[1,1],[1,1]]</code>.');
-assert.deepEqual(operation("m_exp",[[1,2],[3,4]],[[1,2],[3,4]]),[[1,4],[27,256]],'message: <code>operation("m_exp",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[1,4],[27,256]]</code>.');
-assert.deepEqual(operation("m_add",[[1,2,3,4],[5,6,7,8]],[[9,10,11,12],[13,14,15,16]]),[[10,12,14,16],[18,20,22,24]],'message: <code>operation("m_add",[[1,2,3,4],[5,6,7,8]],[[9,10,11,12],[13,14,15,16]])</code> should return <code>[[10,12,14,16],[18,20,22,24]]</code>.');
+assert.deepEqual(operation('m_add', [[1, 2], [3, 4]], [[1, 2], [3, 4]]), [[2, 4], [6, 8]], 'message: <code>operation("m_add",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[2,4],[6,8]]</code>.');
+assert.deepEqual(operation('s_add', [[1, 2], [3, 4]], 2), [[3, 4], [5, 6]], 'message: <code>operation("s_add",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[3,4],[5,6]]</code>.');
+assert.deepEqual(operation('m_sub', [[1, 2], [3, 4]], [[1, 2], [3, 4]]), [[0, 0], [0, 0]], 'message: <code>operation("m_sub",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[0,0],[0,0]]</code>.');
+assert.deepEqual(operation('m_mult', [[1, 2], [3, 4]], [[1, 2], [3, 4]]), [[1, 4], [9, 16]], 'message: <code>operation("m_mult",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[1,4],[9,16]]</code>.');
+assert.deepEqual(operation('m_div', [[1, 2], [3, 4]], [[1, 2], [3, 4]]), [[1, 1], [1, 1]], 'message: <code>operation("m_div",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[1,1],[1,1]]</code>.');
+assert.deepEqual(operation('m_exp', [[1, 2], [3, 4]], [[1, 2], [3, 4]]), [[1, 4], [27, 256]], 'message: <code>operation("m_exp",[[1,2],[3,4]],[[1,2],[3,4]])</code> should return <code>[[1,4],[27,256]]</code>.');
+assert.deepEqual(operation('m_add', [[1, 2, 3, 4], [5, 6, 7, 8]], [[9, 10, 11, 12], [13, 14, 15, 16]]), [[10, 12, 14, 16], [18, 20, 22, 24]], 'message: <code>operation("m_add",[[1,2,3,4],[5,6,7,8]],[[9,10,11,12],[13,14,15,16]])</code> should return <code>[[10,12,14,16],[18,20,22,24]]</code>.');
 /// id: 599c333915e0ea32d04d4bec
