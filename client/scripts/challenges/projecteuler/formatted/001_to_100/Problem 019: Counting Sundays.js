82ebee4a28781132642727da97170c8a6865c80c
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-11-16 08:19:24 +0100

    used ul for problem 19 description

--- client/scripts/challenges/projecteuler/formatted/001_to_100/Problem 019: Counting Sundays.js
+++ client/scripts/challenges/projecteuler/formatted/001_to_100/Problem 019: Counting Sundays.js
@@ -17,15 +17,16 @@ const assert = require('chai').assert;
 //replaceWithActualFunctionHere;
 
 /// description:
-/// <div class="euler"><p class="euler__paragraph">You are given the following information, but you may prefer to do some research for yourself.</p>
-/// <p class="euler__paragraph">1 Jan 1900 was a Monday.</p>
-/// <p class="euler__paragraph">Thirty days has September,</p>
-/// <p class="euler__paragraph">April, June and November.</p>
-/// <p class="euler__paragraph">All the rest have thirty-one,</p>
-/// <p class="euler__paragraph">Saving February alone,</p>
-/// <p class="euler__paragraph">Which has twenty-eight, rain or shine.</p>
-/// <p class="euler__paragraph">And on leap years, twenty-nine.</p>
-/// <p class="euler__paragraph">A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.</p>
+/// <div class="euler">
+/// <p class="euler__paragraph">You are given the following information, but you may prefer to do some research for yourself.</p>
+/// <ul><li>1 Jan 1900 was a Monday.</li>
+/// <li>Thirty days has September,
+/// April, June and November.
+/// All the rest have thirty-one,
+/// Saving February alone,
+/// Which has twenty-eight, rain or shine.
+/// And on leap years, twenty-nine.</li>
+/// <li>A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.</li></ul>
 /// <p class="euler__paragraph">How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?</p></div>
 
 /// challengeSeed:
